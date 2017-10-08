2cf8b4bf11d6d10b9dea4bdb9d692c7724ebf6e4
kengz <kengzwl@gmail.com>
2017-10-08 18:15:49 -0400

    generalize plot_go to 4 main types and restructure generic args

--- unity_lab/lib/viz.py
+++ unity_lab/lib/viz.py
@@ -34,7 +34,7 @@ def create_label(
 
 def create_layout(
         title, y_title, x_title, x_type=None,
-        width=500, height=350):
+        width=500, height=350, layout_kwargs=None):
     '''simplified method to generate Layout'''
     layout = go.Layout(
         title=title,
@@ -44,14 +44,16 @@ def create_layout(
         width=width, height=height,
         margin=go.Margin(l=70, r=70, t=70, b=70),
     )
+    layout.update(layout_kwargs)
     return layout
 
 
-def plot_scatter(
+def plot_go(
         df, y_col, x_col=None,
         title=None, y_title=None, x_title=None, x_type=None,
-        legend_name=None, width=500, height=350, draw=True):
-    '''Draw scatter plot from df'''
+        legend_name=None, width=500, height=350, draw=True,
+        trace_class='Scatter', trace_kwargs=None, layout_kwargs=None):
+    '''Draw plot from df using trace build from specified go.Trace'''
     df = df.copy()
     if x_col is None:
         x_col = 'index'
@@ -62,18 +64,62 @@ def plot_scatter(
     layout = create_layout(
         title=label['title'], y_title=label['y_title'],
         x_title=label['x_title'], x_type=x_type,
-        width=width, height=height)
+        width=width, height=height, layout_kwargs=layout_kwargs)
 
     data = []
     for idx, y_c in enumerate(label['y_col_list']):
         x_c = _.get(label['x_col_list'], idx, default=x_col)
-        trace = go.Scatter(
+        trace = _.get(go, trace_class)(
             y=df[y_c], x=df[x_c],
-            name=label['legend_name_list'][idx],
-        )
+            name=label['legend_name_list'][idx])
+        trace.update(trace_kwargs)
         data.append(trace)
 
     figure = go.Figure(data=data, layout=layout)
     if draw:
         py.iplot(figure)
     return figure
+
+
+def plot_area(*args, **kwargs):
+    '''Plot area from df'''
+    return plot_go(
+        trace_class='Scatter', trace_kwargs=dict(fill='tozeroy'),
+        *args, **kwargs)
+
+
+def plot_bar(
+    *args,
+    barmode='stack', orientation='v', trace_kwargs=None, layout_kwargs=None,
+        **kwargs):
+    '''Plot bar chart from df'''
+    trace_kwargs = _.merge(dict(orientation=orientation), trace_kwargs)
+    layout_kwargs = _.merge(dict(barmode=barmode), layout_kwargs)
+    return plot_go(
+        trace_class='Bar',
+        trace_kwargs=trace_kwargs, layout_kwargs=layout_kwargs,
+        *args, **kwargs)
+
+
+def plot_line(*args, **kwargs):
+    '''Plot scatter from df'''
+    # TODO customize line style
+    return plot_go(trace_class='Scatter', *args, **kwargs)
+
+
+def plot_histogram(
+        *args,
+        barmode='overlay', xbins=None, histnorm='count', orientation='v',
+        trace_kwargs=None, layout_kwargs=None,
+        **kwargs):
+    '''Plot histogram from df'''
+    trace_kwargs = _.merge(dict(orientation=orientation,
+                                xbins={}, histnorm=histnorm), trace_kwargs)
+    layout_kwargs = _.merge(dict(barmode=barmode), layout_kwargs)
+    return plot_go(
+        trace_class='Histogram',
+        trace_kwargs=trace_kwargs, layout_kwargs=layout_kwargs,
+        *args, **kwargs)
+
+# do not default x or y, take 'index' arg
+# scatter, pie, swarm, box
