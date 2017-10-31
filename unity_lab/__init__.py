07d06d790b0ab5dce36314260f4352d796354e27
kengz <kengzwl@gmail.com>
2017-10-30 22:38:41 -0400

    remove cufflinks, update plotly config, add save_image logic

---
+++ unity_lab/__init__.py
@@ -0,0 +1,12 @@
+import json
+import os
+
+os.environ['PY_ENV'] = os.environ.get('PY_ENV') or 'development'
+CONFIG_NAME_MAP = {
+    'development': 'default',
+    'production': 'production',
+}
+
+config_name = CONFIG_NAME_MAP.get(os.environ['PY_ENV'])
+with open(os.path.join(os.getcwd(), f'config/{config_name}.json')) as f:
+    config = json.load(f)
