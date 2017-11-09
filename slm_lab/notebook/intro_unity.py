41591d54a3ebb01bab045fbcf7f6a1187b20c0ed
kengz <kengzwl@gmail.com>
2017-11-08 20:12:40 -0500

    update intro unity doc

--- slm_lab/notebook/intro_unity.py
+++ slm_lab/notebook/intro_unity.py
@@ -1,23 +1,22 @@
 '''
 Introduction to Unity ml-agents
 
-Install Unity ml-agents in lab Conda env
+Refer to README for setup
+
 if you're live-hacking stuff, use `pip install -e .`
 https://github.com/Unity-Technologies/ml-agents/blob/master/docs/installation.md
 
 ml-agents Python API doc:
 https://github.com/Unity-Technologies/ml-agents/blob/master/docs/Unity-Agents---Python-API.md
 '''
-# TODO update doc above
 
 import numpy as np
 import time
 from slm_lab.lib import util
 from unityagents import UnityEnvironment
 
-# TODO util method to infer path of environment, plus gitignore the file
-# env_name = f'{util.ENV_DIR}/Build/3dball'
-env_name = f'./node_modules/slm-env-gridworld/build/gridworld'
+# TODO update env pathing from util var
+env_name = f'./node_modules/slm-env-3dball/build/3dball'
 # use train_mode = False to debug, i.e. render env at real size, real time
 train_mode = False
 
