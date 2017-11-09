b5bf0e5eb1ddef22615e62b90a12898bdd639be9
lgraesser <lgraesser@users.noreply.github.com>
2017-11-09 13:23:19 -0500

    General convnet implementation (#9)
    * General convnet implementation
    
    * Unit tests for conv nets
    
    * Updating convnet tests
    
    * Fixing convnet unit tests
    
    * Adding more examples to convnet unit tests
    
    * Adding replay memory import
    
    * Style fixes
    
    * Removing print statements from test

---
+++ slm_lab/agent/net/quick_test.py
@@ -0,0 +1,62 @@
+import torch
+import torch.nn as nn
+from torch import optim
+from convnet import ConvNet
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+
+dummy_input = Variable(torch.ones((2, 3, 32, 32)))
+net = ConvNet((3, 32, 32),
+              [[3, 16, (5, 5), 2, 0, 1],
+               [16, 32, (5, 5), 2, 0, 1]],
+              [100, 50],
+              10,
+              optim.Adam,
+              F.smooth_l1_loss,
+              False,
+              False)
+
+print(net)
+out = net(dummy_input)
+
+net = ConvNet((3, 32, 32),
+              [[3, 16, (5, 5), 2, 0, 1],
+               [16, 32, (5, 5), 2, 0, 1]],
+              [100, 50],
+              10,
+              optim.Adam,
+              F.smooth_l1_loss,
+              False,
+              True)
+
+print(net)
+out = net(dummy_input)
+
+net = ConvNet((3, 32, 32),
+              [[3, 16, (7, 7), 1, 0, 1],
+               [16, 32, (5, 5), 1, 0, 1],
+               [32, 64, (3, 3), 1, 0, 1]],
+              [100, 50],
+              10,
+              optim.Adam,
+              F.smooth_l1_loss,
+              False,
+              False)
+
+print(net)
+out = net(dummy_input)
+
+net = ConvNet((3, 32, 32),
+              [[3, 16, (7, 7), 1, 0, 1],
+               [16, 32, (5, 5), 1, 0, 1],
+               [32, 64, (3, 3), 1, 0, 1]],
+              [100, 50],
+              10,
+              optim.Adam,
+              F.smooth_l1_loss,
+              False,
+              True)
+
+print(net)
+out = net(dummy_input)
