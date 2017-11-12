301f91cec3e656a85625c34fab7c131846b18d96
lgraesser <lhgraesser@gmail.com>
2017-11-12 18:43:55 -0500

    Adding comments, style fixes

--- slm_lab/agent/algorithms/dqn.py
+++ slm_lab/agent/algorithms/dqn.py
@@ -9,8 +9,11 @@ class DQN:
 
     net: instance of an slm_lab/agent/net
     memory: instance of an slm_lab/agent/memory
-    batch_size: how many examples from memory to sample for each training step
-    action_selection: function (from common.py). Determines how
+    batch_size: how many examples from memory to sample at each training step
+    action_selection: function (from common.py) that determines how to select
+                      actions
+    gamma: Real number in range [0, 1]. Determines how much to discount the
+           future
     '''
 
     def __init__(self,
@@ -35,14 +38,16 @@ class DQN:
     def compute_q_target_values(self, batch):
         q_vals = self.net.eval(batch['states'])
         # Make future reward 0 if the current state is terminal
-        q_targets_all = batch['rewards'] + self.gamma * (1 - batch['terminals'])  * self.net.eval(batch['next_states'])
+        q_targets_all = batch['rewards'] + self.gamma * \
+            (1 - batch['terminals'])  * self.net.eval(batch['next_states'])
         q_targets_max = torch.max(q_targets_all, axis=1)
         # Reshape q_targets_max to q_targets all shape
         q_targets_max = q_targets_max.expand(-1, q_targets_all.shape[1])
         # We only want to train the network for the action selected
         # For all other actions we set the q_target = q_vals
         # So that the loss for these actions is 0
-        q_targets = torch.mul(q_targets_max, batch['actions']) + torch.mul(q_vals, (1 - batch['actions']))
+        q_targets = torch.mul(q_targets_max, batch['actions']) + \
+                    torch.mul(q_vals, (1 - batch['actions']))
         return q_targets
 
     def select_action(self, state, epsilon_or_tau):
