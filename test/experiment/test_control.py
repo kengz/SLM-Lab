99374d56e1aa366c7d374ce2d7c8d204421520b1
kengz <kengzwl@gmail.com>
2017-11-19 23:14:47 -0600

    add basic experiment control tests

---
+++ test/experiment/test_control.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import pytest
+from slm_lab import spec
+from slm_lab.experiment.control import Monitor, Session, Trial
+
+
+def test_session(test_exp_spec):
+    monitor = Monitor(test_exp_spec)
+    session = Session(test_exp_spec, monitor)
+    session_data = session.run()
+    # TODO session data checker method
+    assert isinstance(session_data, pd.DataFrame)
+
+
+def test_trial(test_exp_spec):
+    monitor = Monitor(test_exp_spec)
+    trial = Trial(test_exp_spec, monitor)
+    trial_data = trial.run()
+    # TODO trial data checker method
+    assert isinstance(trial_data, pd.DataFrame)
+
+
+def test_experiment():
+    return
