c0c63041faafab96a32e2205fdf772b9f9d979b3
kengz <kengzwl@gmail.com>
2017-11-26 22:31:17 -0500

    param spec test

--- test/spec/test_spec_util.py
+++ test/spec/test_spec_util.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 from slm_lab.spec import spec_util
 
 
@@ -16,34 +17,31 @@ def test_get():
     assert spec is not None
 
 
-def test_resolve_aeb():
-    inner_spec = spec_util.get('base.json', 'general_inner')
-    inner_aeb_coor_list = spec_util.resolve_aeb(inner_spec)
-    assert inner_aeb_coor_list == [(0, 0, 0), (0, 0, 1), (1, 1, 0), (1, 1, 1)]
-
-    outer_spec = spec_util.get('base.json', 'general_outer')
-    outer_aeb_coor_list = spec_util.resolve_aeb(outer_spec)
-    assert outer_aeb_coor_list == [(0, 0, 0),
-                                   (0, 0, 1),
-                                   (0, 1, 0),
-                                   (0, 1, 1),
-                                   (1, 0, 0),
-                                   (1, 0, 1),
-                                   (1, 1, 0),
-                                   (1, 1, 1)]
-
-    custom_spec = spec_util.get('base.json', 'general_custom')
-    custom_aeb_coor_list = spec_util.resolve_aeb(custom_spec)
-    assert custom_aeb_coor_list == [(0, 0, 0),
-                                    (0, 1, 0),
-                                    (0, 1, 1),
-                                    (0, 1, 2),
-                                    (0, 1, 3),
-                                    (0, 1, 4),
-                                    (0, 1, 5),
-                                    (0, 1, 6),
-                                    (0, 1, 7),
-                                    (0, 1, 8),
-                                    (0, 1, 9),
-                                    (0, 1, 10),
-                                    (0, 1, 11)]
+@pytest.mark.parametrize('spec_name,aeb_coor_list', [
+    ('general_inner', [(0, 0, 0), (0, 0, 1), (1, 1, 0), (1, 1, 1)]),
+    ('general_outer', [(0, 0, 0),
+                       (0, 0, 1),
+                       (0, 1, 0),
+                       (0, 1, 1),
+                       (1, 0, 0),
+                       (1, 0, 1),
+                       (1, 1, 0),
+                       (1, 1, 1)]),
+    ('general_custom', [(0, 0, 0),
+                        (0, 1, 0),
+                        (0, 1, 1),
+                        (0, 1, 2),
+                        (0, 1, 3),
+                        (0, 1, 4),
+                        (0, 1, 5),
+                        (0, 1, 6),
+                        (0, 1, 7),
+                        (0, 1, 8),
+                        (0, 1, 9),
+                        (0, 1, 10),
+                        (0, 1, 11)]),
+])
+def test_resolve_aeb(spec_name, aeb_coor_list):
+    spec = spec_util.get('base.json', spec_name)
+    resolved_aeb_coor_list = spec_util.resolve_aeb(spec)
+    assert resolved_aeb_coor_list == aeb_coor_list
