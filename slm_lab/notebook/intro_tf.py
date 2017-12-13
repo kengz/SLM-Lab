bc16fcf38d7e1c3e301e1f085bb5005293f7ba36
kengz <kengzwl@gmail.com>
2017-12-12 20:57:14 -0500

    commit convnet mnist example

--- slm_lab/notebook/intro_tf.py
+++ slm_lab/notebook/intro_tf.py
@@ -113,3 +113,91 @@ for _ in range(1000):
 is_correct = tf.equal(tf.argmax(y, axis=1), tf.argmax(y_, axis=1))
 accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
 sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels})
+
+
+# Conv MNIST
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets('data/mnist/', one_hot=True, reshape=False)
+
+# to build graph on the fly
+sess = tf.InteractiveSession()
+
+x = tf.placeholder(tf.float32, shape=[None, 28, 28, 1])
+y_ = tf.placeholder(tf.float32, shape=[None, 10])
+lr = tf.placeholder(tf.float32)
+pkeep = tf.placeholder(tf.float32)
+
+
+def weight_variable(shape):
+    return tf.Variable(tf.truncated_normal(shape, stddev=0.1))
+
+
+def bias_variable(shape):
+    return tf.Variable(tf.constant(0.1, shape=shape))
+
+
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+
+# the model
+# channels
+c0 = 1
+c1 = 6
+c2 = 12
+fc_size = 200  # fully connected before output
+stride = 1
+
+W1 = weight_variable([6, 6, c0, c1])
+b1 = bias_variable([c1])
+
+h_conv1 = tf.nn.relu(conv2d(x, W1) + b1)
+h_pool1 = max_pool_2x2(h_conv1)
+
+W2 = weight_variable([5, 5, c1, c2])
+b2 = bias_variable([c2])
+
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W2) + b2)
+h_pool2 = max_pool_2x2(h_conv2)
+
+# fully connected
+W3 = weight_variable([7 * 7 * c2, fc_size])
+b3 = bias_variable([fc_size])
+
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * c2])
+h_3 = tf.nn.relu(tf.matmul(h_pool2_flat, W3) + b3)
+
+# dropout
+h_drop = tf.nn.dropout(h_3, pkeep)
+
+W_out = weight_variable([fc_size, 10])
+b_out = bias_variable([10])
+
+y_conv = tf.matmul(h_drop, W_out) + b_out
+
+cross_entropy = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
+optimizer = tf.train.AdamOptimizer(lr)
+train = optimizer.minimize(cross_entropy)
+is_correct = tf.equal(tf.argmax(y_conv, axis=1), tf.argmax(y_, axis=1))
+accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
+
+init = tf.global_variables_initializer()
+with tf.Session() as sess:
+    sess.run(init)
+    for i in range(2000):
+        batch = mnist.train.next_batch(100)
+        if i % 100 == 0:
+            train_accuracy = accuracy.eval(
+                feed_dict={x: batch[0], y_: batch[1], pkeep: 1.0})
+            print(f'train acc: {i}, {train_accuracy}')
+        train.run(feed_dict={x: batch[0], y_: batch[1], pkeep: 0.5, lr: 0.01})
+
+    train_accuracy = accuracy.eval(
+        feed_dict={x: batch[0], y_: batch[1], pkeep: 1.0})
+    print(f'train acc: {i}, {train_accuracy}')
