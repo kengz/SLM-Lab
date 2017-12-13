9451ca23b675a8dc1b19b0444cccf4c03b7e4452
kengz <kengzwl@gmail.com>
2017-12-13 00:10:21 -0500

    improve unity worker_id hash

--- slm_lab/env/__init__.py
+++ slm_lab/env/__init__.py
@@ -144,8 +144,9 @@ class Env:
         self.index = e
         self.ab_proj = self.env_space.e_ab_proj[self.index]
         self.bodies = None  # consistent with ab_proj, set in aeb_space.init_body_space()
+        worker_id = int(str(os.getpid() + self.index)[-2:])
         self.u_env = UnityEnvironment(
-            file_name=util.get_env_path(self.name), worker_id=os.getpid() + self.index)
+            file_name=util.get_env_path(self.name), worker_id=worker_id)
         self.check_u_brain_to_agent()
 
     def check_u_brain_to_agent(self):
