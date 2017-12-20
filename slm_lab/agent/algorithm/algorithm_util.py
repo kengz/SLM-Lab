d8a50061ad0f00ba025d7ba1ec6054d67dcacb40
lgraesser <lhgraesser@gmail.com>
2017-12-20 16:46:55 -0500

    Adding act_with_softmax for reinforce

--- slm_lab/agent/algorithm/algorithm_util.py
+++ slm_lab/agent/algorithm/algorithm_util.py
@@ -2,10 +2,11 @@
 Functions used by more than one algorithm
 TODO refactor properly later
 '''
-from torch.autograd import Variable
 import numpy as np
 import torch
 import torch.nn.functional as F
+from torch.autograd import Variable
+from torch.distributions import Categorical
 
 
 def act_with_epsilon_greedy(body, state, net, epsilon):
@@ -78,22 +79,31 @@ def multi_act_with_boltzmann(bodies, state, net, tau):
         start_idx = end_idx
     return action
 
+# Adapted from  https://github.com/pytorch/examples/blob/master/reinforcement_learning/reinforce.py
+def act_with_softmax(body, state, net):
+    torch_state = Variable(torch.from_numpy(state).float())
+    out = net.wrap_eval(torch_state)
+    probs = F.softmax(out)
+    m = Categorical(probs)
+    action = m.sample()
+    net.saved_log_probs.append(m.log_prob(action))
+    return action.data[0]
 
 def act_with_gaussian(body, state, net, stddev):
     # TODO implement act_with_gaussian
     pass
 
-
+# TODO - do we need these? Update handled in the agent
 def update_epsilon_greedy(body, state, net, stddev):
     # TODO implement act_with_gaussian
     pass
 
-
+# TODO - do we need these? Update handled in the agent
 def update_boltzmann(body, state, net, stddev):
     # TODO implement act_with_gaussian
     pass
 
-
+# TODO - do we need these? Update handled in the agent
 def update_gaussian(body, state, net, stddev):
     # TODO implement act_with_gaussian
     pass
