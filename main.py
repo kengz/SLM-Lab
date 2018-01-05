077eaf87e9acece81ca09277ecab4bba85e580ce
kengz <kengzwl@gmail.com>
2018-01-04 22:57:50 -0500

    add demo spec

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from slm_lab.experiment import analysis
 from slm_lab.experiment.control import Session, Trial
 from slm_lab.lib import logger, util
 from slm_lab.spec import spec_util
@@ -6,12 +5,8 @@ from slm_lab.spec import spec_util
 
 def main():
     # logger.set_level('DEBUG')
-    spec = spec_util.get('dqn.json', 'dqn_cartpole')
-    # spec = spec_util.get('dqn.json', 'dqn_cartpole_cartpole')
-    # spec = spec_util.get('dqn.json', 'dqn_cartpole_cartpole_cartpole')
-    # spec = spec_util.get('dqn.json', 'dqn_acrobot_cartpole')
-    # spec = spec_util.get('dqn.json', 'dqn_2dball_cartpole')
-    Session(spec).run()
+    spec = spec_util.get('demo.json', 'dqn_cartpole')
+    Trial(spec).run()
 
 
 if __name__ == '__main__':
