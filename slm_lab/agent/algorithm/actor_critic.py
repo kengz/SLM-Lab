59af46d5123a40dd6abcf298b4d4414520f9841c
lgraesser <lhgraesser@gmail.com>
2018-01-18 17:09:54 -0500

    Automatic discrete / continuous switching for AC

--- slm_lab/agent/algorithm/actor_critic.py
+++ slm_lab/agent/algorithm/actor_critic.py
@@ -6,6 +6,7 @@ from slm_lab.agent.net import net_util
 from slm_lab.lib import util, logger
 from slm_lab.lib.decorator import lab_api
 from torch.autograd import Variable
+import sys
 import numpy as np
 import torch
 import pydash as _
@@ -33,39 +34,64 @@ class ActorCritic(Reinforce):
         self.init_algo_params()
         self.actor.print_nets()  # Print the network architecture
         logger.info(util.self_desc(self))
+        sys.exit()
 
     def init_nets(self):
         '''Initialize the neural network used to learn the Q function from the spec'''
         body = self.agent.nanflat_body_a[0]  # singleton algo
         state_dim = body.state_dim
         action_dim = body.action_dim
-        # TODO: create separate net specs
-        # TODO: automate actor switch between continuous and discrete
+        self.is_discrete = body.is_discrete
         net_spec = self.agent.spec['net']
+        net_type = self.agent.spec['net']['type']
         actor_kwargs = util.compact_dict(dict(
             hid_layers_activation=_.get(net_spec, 'hid_layers_activation'),
             optim_param=_.get(net_spec, 'optim_actor'),
-            loss_param=_.get(net_spec, 'loss'),  # Note: Not used for PG algos
+            loss_param=_.get(net_spec, 'loss'),  # Note: Not used for training actor
             clamp_grad=_.get(net_spec, 'clamp_grad'),
             clamp_grad_val=_.get(net_spec, 'clamp_grad_val'),
         ))
-        self.actor = getattr(net, net_spec['type'])(
-            state_dim, net_spec['hid_layers'], action_dim, **actor_kwargs)
         critic_kwargs = util.compact_dict(dict(
             hid_layers_activation=_.get(net_spec, 'hid_layers_activation'),
             optim_param=_.get(net_spec, 'optim_critic'),
-            loss_param=_.get(net_spec, 'loss'),  # Note: Not used for PG algos
+            loss_param=_.get(net_spec, 'loss'),
             clamp_grad=_.get(net_spec, 'clamp_grad'),
             clamp_grad_val=_.get(net_spec, 'clamp_grad_val'),
         ))
-        self.critic = getattr(net, net_spec['type'])(
-            state_dim, net_spec['hid_layers'], 1, **critic_kwargs)
+        # Below we automatically select an appropriate net based on two different conditions
+        #   1. If the action space is discrete or continuous action
+        #           - Networks for continuous action spaces have two heads and return two values, the first is a tensor containing the mean of the action policy, the second is a tensor containing the std deviation of the action policy. The distribution is assumed to be a Gaussian (Normal) distribution.
+        #           - Networks for discrete action spaces have a single head and return the logits for a categorical probability distribution over the discrete actions
+        #   2. If the actor and critic should be separate or share weights
+        #           - If the networks share weights then the single network returns a tuple. The first element contains the output for the actor (policy). This will vary depending on the type of distribution (see point 1.). The second element contains the state-value estimated by the network.
+        if net_type == 'MLPseparate':
+            self.is_shared_architecture = False
+            if self.is_discrete:
+                self.actor = getattr(net, 'MLPNet')(
+                    state_dim, net_spec['hid_layers'], action_dim, **actor_kwargs)
+            else:
+                self.actor = getattr(net, 'MLPHeterogenousHeads')(
+                    state_dim, net_spec['hid_layers'], [action_dim, action_dim], **actor_kwargs)
+            self.critic = getattr(net, 'MLPNet')(
+                state_dim, net_spec['hid_layers'], 1, **critic_kwargs)
+        else:
+            self.is_shared_architecture = True
+            # TODO fix for MLPshared - single heterogenous head
+            logger.info(f'Net type {net_type} not supported yet. Please use MLPseparate')
+            sys.exit()
 
     def init_algo_params(self):
         '''Initialize other algorithm parameters'''
         algorithm_spec = self.agent.spec['algorithm']
-        # TODO: fix for auto continuous and discrete adjustment
-        self.action_policy = act_fns[algorithm_spec['action_policy']]
+        # Automatically selects appropriate discrete or continuous action policy if setting is default
+        action_fn = algorithm_spec['action_policy']
+        if action_fn == 'default':
+            if self.is_discrete:
+                self.action_policy = act_fns['softmax']
+            else:
+                self.action_policy = act_fns['gaussian']
+        else:
+            self.action_policy = act_fns[action_fn]
         util.set_attr(self, _.pick(algorithm_spec, [
             'gamma',
             'training_frequency', 'training_iters_per_batch',
@@ -90,7 +116,6 @@ class ActorCritic(Reinforce):
 
     def sample(self):
         '''Samples a batch from memory'''
-        # TODO: Change sampling type depending on memory
         batches = [body.memory.sample()
                    for body in self.agent.nanflat_body_a]
         batch = util.concat_dict(batches)
@@ -99,6 +124,12 @@ class ActorCritic(Reinforce):
 
     @lab_api
     def train(self):
+        if self.is_shared_architecture:
+            return self.train_shared()
+        else:
+            return self.train_separate()
+
+    def train_separate(self):
         if self.to_train == 1:
             batch = self.sample()
             if len(batch['states']) < self.training_frequency:
@@ -124,6 +155,7 @@ class ActorCritic(Reinforce):
 
     def calc_advantage(self, batch):
         target = self.get_target(batch)
+        # TODO option for batch and episodic memory
         state_vals = self.critic.wrap_eval(batch['states']).squeeze_()
         advantage = target - state_vals
         advantage.squeeze_()
@@ -131,15 +163,20 @@ class ActorCritic(Reinforce):
         return advantage
 
     def gae_0_target(self, batch):
+        # TODO explain gae_0_target
         next_state_vals = self.critic.wrap_eval(
             batch['next_states']).squeeze_()
         target = batch['rewards'].data + self.gamma * \
             torch.mul((1 - batch['dones'].data), next_state_vals)
         logger.debug(f'Target: {target.size()}')
+        # TODO add option for episodic memory
         return target
 
     def gae_1_target(self, batch):
+        # TODO explain gae_1_target
+        # TODO add memory guard
         rewards = []
+        # TODO fix for multiple episodes
         epi_rewards = batch['rewards']
         big_r = 0
         for i in xrange(epi_rewards.size(0), 0, -1):
@@ -164,7 +201,8 @@ class ActorCritic(Reinforce):
         assert len(self.entropy) == advantage.size(0)
         policy_loss = []
         for log_prob, a, e in zip(self.saved_log_probs, advantage, self.entropy):
-            logger.debug(f'log prob: {log_prob.data[0]}, advantage: {a}, entropy: {e.data[0]}')
+            logger.debug(
+                f'log prob: {log_prob.data[0]}, advantage: {a}, entropy: {e.data[0]}')
             if self.add_entropy:
                 policy_loss.append(-log_prob * a - 0.1 * e)
             else:
@@ -184,3 +222,7 @@ class ActorCritic(Reinforce):
         self.entropy = []
         logger.debug(f'Policy loss: {loss}')
         return loss
+
+    def train_shared(self):
+        # TODO: implement train_shared
+        pass
