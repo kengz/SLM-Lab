2b0113f01cf202688b71d2734938c8a28d74de20
kengz <kengzwl@gmail.com>
2018-01-21 15:10:41 -0500

    update usage instruction

--- run_lab.py
+++ run_lab.py
@@ -1,17 +1,41 @@
+'''
+The entry point of SLM Lab
+Specify what to run in `config/experiments.json`
+Then run `yarn start` or `python run_lab.py`
+'''
 from slm_lab.experiment.control import Session, Trial, Experiment
 from slm_lab.lib import logger, util
 from slm_lab.spec import spec_util
 
 
+def run_by_mode(spec_file, spec_name, run_mode):
+    spec = spec_util.get(spec_file, spec_name)
+    if run_mode == 'search':
+        Experiment(spec).run()
+    elif run_mode == 'train':
+        Trial(spec).run()
+    elif run_mode == 'enjoy':
+        # TODO turn on save/load model mode
+        # Session(spec).run()
+        pass
+    elif run_mode == 'benchmark':
+        # TODO need to spread benchmark over spec on Experiment
+        pass
+    elif run_mode == 'dev':
+        logger.set_level('DEBUG')
+        spec['meta']['train_mode'] = True
+        spec['meta']['max_episode'] = 20
+        Trial(spec).run()
+    else:
+        logger.warn(
+            'run_mode not recognized; must be one of `search, train, enjoy, benchmark, dev`.')
+
+
 def main():
-    # TODO streamline to grunt from external in the next release
-    # logger.set_level('DEBUG')
-    spec = spec_util.get('demo.json', 'dqn_cartpole')
-    Trial(spec).run()
-    # spec = spec_util.get('dqn.json', 'dqn_cartpole')
-    # spec = spec_util.get('reinforce.json', 'reinforce_cartpole')
-    # spec = spec_util.get('actor_critic.json', 'actor_critic_cartpole')
-    # Experiment(spec).run()
+    experiments = util.read('config/experiments.json')
+    for spec_file in experiments:
+        for spec_name, run_mode in experiments[spec_file].items():
+            run_by_mode(spec_file, spec_name, run_mode)
 
 
 if __name__ == '__main__':
