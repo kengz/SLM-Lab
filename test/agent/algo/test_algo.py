df402210145a14472c8a52bc1f156c8710682e5f
lgraesser <lhgraesser@gmail.com>
2018-02-01 16:03:51 -0500

    Refactoring test_algo

--- test/agent/algo/test_algo.py
+++ test/agent/algo/test_algo.py
@@ -7,10 +7,7 @@ import os
 import shutil
 
 
-@flaky(max_runs=5)
-def test_algo(test_algorithms):
-    algo_name = test_algorithms
-    spec = spec_util.get('test.json', algo_name)
+def generic_algo_test(spec, algo_name):
     trial_data = Trial(spec).run()
     folders = [x for x in os.listdir('data/') if x.startswith(algo_name)]
     assert len(folders) == 1
@@ -21,4 +18,35 @@ def test_algo(test_algorithms):
     maxr = rewards.max()
     '''Delete test data folder'''
     shutil.rmtree(path)
-    assert maxr > 100
+    return maxr
+
+
+def get_dqn_spec():
+    return spec_util.get('test.json', 'unit_test_dqn')
+
+
+# @flaky(max_runs=3)
+# def test_dqn():
+#     algo_name = 'unit_test_dqn'
+#     spec = get_dqn_spec()
+#     assert generic_algo_test(spec, algo_name) > 100
+
+
+# @flaky(max_runs=3)
+def test_vanilla_dqn():
+    algo_name = 'unit_test_vanilla_dqn'
+    spec = get_dqn_spec()
+    spec['name'] = algo_name
+    spec['agent'][0]['algorithm']['name'] = "VanillaDQN"
+    print(spec)
+    assert True is False
+    assert generic_algo_test(spec, algo_name) > 100
+
+
+# @flaky(max_runs=3)
+# def test_double_dqn():
+#     algo_name = 'unit_test_double_dqn'
+#     spec = get_dqn_spec()
+#     spec['name'] = algo_name
+#     spec['agent'][0]['algorithm']['name'] = "DoubleDQN"
+#     assert generic_algo_test(spec, algo_name) > 100
