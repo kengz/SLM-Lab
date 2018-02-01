8b53e8f749a4eb3e471356b28d8bdcd176736e91
kengz <kengzwl@gmail.com>
2018-01-31 21:11:15 -0500

    unify logger, fix init, write to target file

--- slm_lab/lib/logger.py
+++ slm_lab/lib/logger.py
@@ -5,8 +5,6 @@ import os
 import sys
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # mute tf warnings on optimized setup
-LOG_FILEPATH = util.smart_path(
-    f'log/{util.get_ts()}_slm_lab.log')
 LOG_FORMAT = '[%(asctime)s %(levelname)s] %(message)s'
 LOG_LEVEL = logging.DEBUG if bool(os.environ.get('DEBUG')) else logging.INFO
 
@@ -19,18 +17,44 @@ class DedentFormatter(logging.Formatter):
         return super(DedentFormatter, self).format(record)
 
 
-os.makedirs(os.path.dirname(LOG_FILEPATH), exist_ok=True)
 color_formatter = colorlog.ColoredFormatter(
     '%(log_color)s[%(asctime)s %(levelname)s]%(reset)s %(message)s')
-fh = logging.FileHandler(LOG_FILEPATH)
 sh = logging.StreamHandler(sys.stdout)
 sh.setFormatter(color_formatter)
-
-lab_logger = logging.getLogger('slm')
+lab_logger = logging.getLogger()
 lab_logger.setLevel(LOG_LEVEL)
-lab_logger.addHandler(fh)
 lab_logger.addHandler(sh)
 lab_logger.propagate = False
+
+
+if os.environ.get('prepath') is not None:
+    # this will trigger from Experiment init on reload(logger)
+    # mute the competing loggers
+    logging.getLogger('gym').setLevel(logging.WARN)
+    logging.getLogger('requests').setLevel(logging.WARN)
+    logging.getLogger('unityagents').setLevel(logging.WARN)
+
+    log_filepath = os.environ['prepath'].replace('data/', 'log/') + '.log'
+    os.makedirs(os.path.dirname(log_filepath), exist_ok=True)
+    # create file handler
+    formatter = logging.Formatter(LOG_FORMAT)
+    fh = logging.FileHandler(log_filepath)
+    fh.setFormatter(formatter)
+    # remove old handlers to prevent repeated logging
+    for handler in lab_logger.handlers[:]:
+        lab_logger.removeHandler(handler)
+    # add stream and file handler
+    lab_logger.addHandler(sh)
+    lab_logger.addHandler(fh)
+
+
+def to_init(info_space, spec):
+    '''
+    Whether the lab's logger had been initialized:
+    - prepath present in env
+    - importlib.reload(logger) had been called
+    '''
+    return os.environ.get('prepath') is None
 
 
 def set_level(lvl):
