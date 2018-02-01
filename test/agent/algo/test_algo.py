4993f47c6410eefd68aa0f5877c352795e5ea6e1
lgraesser <lhgraesser@gmail.com>
2018-02-01 16:51:46 -0500

    All dqn unit tests

--- test/agent/algo/test_algo.py
+++ test/agent/algo/test_algo.py
@@ -1,5 +1,6 @@
+from slm_lab.experiment.monitor import InfoSpace
 from slm_lab.experiment.control import Session, Trial, Experiment
-from slm_lab.lib import util
+from slm_lab.lib import util, logger
 from slm_lab.spec import spec_util
 from flaky import flaky
 import pytest
@@ -8,7 +9,9 @@ import shutil
 
 
 def generic_algo_test(spec, algo_name):
-    trial_data = Trial(spec).run()
+    '''Need new InfoSpace() per trial otherwise session id doesn't tick correctly'''
+    trial = Trial(spec, info_space=InfoSpace())
+    trial_data = trial.run()
     folders = [x for x in os.listdir('data/') if x.startswith(algo_name)]
     assert len(folders) == 1
     path = 'data/' + folders[0]
@@ -16,37 +19,32 @@ def generic_algo_test(spec, algo_name):
     rewards = sess_data['0.2'].replace("reward", -1).astype(float)
     print(f'rewards: {rewards}')
     maxr = rewards.max()
-    '''Delete test data folder'''
+    '''Delete test data folder and trial'''
     shutil.rmtree(path)
+    del trial, spec
     return maxr
 
 
-def get_dqn_spec():
-    return spec_util.get('test.json', 'unit_test_dqn')
-
-
-# @flaky(max_runs=3)
-# def test_dqn():
-#     algo_name = 'unit_test_dqn'
-#     spec = get_dqn_spec()
-#     assert generic_algo_test(spec, algo_name) > 100
-
-
-# @flaky(max_runs=3)
-def test_vanilla_dqn():
-    algo_name = 'unit_test_vanilla_dqn'
-    spec = get_dqn_spec()
-    spec['name'] = algo_name
-    spec['agent'][0]['algorithm']['name'] = "VanillaDQN"
-    print(spec)
-    assert True is False
+@flaky(max_runs=3)
+def test_dqn():
+    algo_name = 'unit_test_dqn'
+    spec = spec_util.get('test.json', 'unit_test_dqn')
     assert generic_algo_test(spec, algo_name) > 100
 
 
-# @flaky(max_runs=3)
-# def test_double_dqn():
-#     algo_name = 'unit_test_double_dqn'
-#     spec = get_dqn_spec()
-#     spec['name'] = algo_name
-#     spec['agent'][0]['algorithm']['name'] = "DoubleDQN"
-#     assert generic_algo_test(spec, algo_name) > 100
+@flaky(max_runs=3)
+def test_vanilla_dqn():
+    algo_name = 'unit_test_vanilla_dqn'
+    spec = spec_util.get('test.json', 'unit_test_dqn')
+    spec['name'] = algo_name
+    spec['agent'][0]['algorithm']['name'] = "VanillaDQN"
+    assert generic_algo_test(spec, algo_name) > 100
+
+
+@flaky(max_runs=3)
+def test_double_dqn():
+    algo_name = 'unit_test_double_dqn'
+    spec = spec_util.get('test.json', 'unit_test_dqn')
+    spec['name'] = algo_name
+    spec['agent'][0]['algorithm']['name'] = "DoubleDQN"
+    assert generic_algo_test(spec, algo_name) > 100
