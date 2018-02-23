6365225f5eee36cc8da6432adfa310bcbab2448d
kengz <kengzwl@gmail.com>
2018-02-22 19:35:03 -0500

    only early stop on random agent

--- slm_lab/experiment/monitor.py
+++ slm_lab/experiment/monitor.py
@@ -240,7 +240,7 @@ class AEBSpace:
                     aeb_fitness_sr = analysis.calc_aeb_fitness_sr(
                         aeb_df, body.env.name)
                     strength = aeb_fitness_sr['strength']
-                    env_early_stop = strength < 0.5
+                    env_early_stop = strength < analysis.NOISE_WINDOW
                 else:
                     env_early_stop = False
                 env_early_stops.append(env_early_stop)
