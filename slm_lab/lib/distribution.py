f427b2a23699b7d6c3903e0563bab1ff3b41ef52
kengz <kengzwl@gmail.com>
2018-03-10 14:55:55 -0500

    adapt distribution module from openai baselines

---
+++ slm_lab/lib/distribution.py
@@ -0,0 +1,289 @@
+'''
+The probability distribution module in tensorflow
+adapted from https://github.com/openai/baselines/blob/master/baselines/common/distributions.py
+'''
+
+from abc import ABC
+from tensorflow.python.ops import math_ops
+import numpy as np
+import tensorflow as tf
+
+
+class PD(ABC):
+    '''
+    A particular probability distribution
+    '''
+
+    def flatparam(self):
+        raise NotImplementedError
+
+    def mode(self):
+        raise NotImplementedError
+
+    def neglogp(self, x):
+        # Usually it's easier to define the negative logprob
+        raise NotImplementedError
+
+    def logp(self, x):
+        return - self.neglogp(x)
+
+    def kl(self, other):
+        raise NotImplementedError
+
+    def entropy(self):
+        raise NotImplementedError
+
+    def sample(self):
+        raise NotImplementedError
+
+
+class PDType(ABC):
+    '''
+    Parametrized family of probability distributions
+    '''
+
+    def pdclass(self):
+        raise NotImplementedError
+
+    def pdfromflat(self, flat):
+        return self.pdclass()(flat)
+
+    def param_shape(self):
+        raise NotImplementedError
+
+    def sample_shape(self):
+        raise NotImplementedError
+
+    def sample_dtype(self):
+        raise NotImplementedError
+
+    def param_placeholder(self, prepend_shape, name=None):
+        return tf.placeholder(
+            dtype=tf.float32, shape=prepend_shape + self.param_shape(), name=name)
+
+    def sample_placeholder(self, prepend_shape, name=None):
+        return tf.placeholder(
+            dtype=self.sample_dtype(), shape=prepend_shape + self.sample_shape(), name=name)
+
+
+class CategoricalPd(PD):
+    def __init__(self, logits):
+        self.logits = logits
+
+    def flatparam(self):
+        return self.logits
+
+    def mode(self):
+        return tf.argmax(self.logits, axis=-1)
+
+    def neglogp(self, x):
+        # return tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.logits, labels=x)
+        # Note: we can't use sparse_softmax_cross_entropy_with_logits because
+        #       the implementation does not allow second-order derivatives...
+        one_hot_actions = tf.one_hot(x, self.logits.get_shape().as_list()[-1])
+        return tf.nn.softmax_cross_entropy_with_logits(
+            logits=self.logits, labels=one_hot_actions)
+
+    def kl(self, other):
+        a0 = self.logits - tf.reduce_max(self.logits, axis=-1, keepdims=True)
+        a1 = other.logits - tf.reduce_max(other.logits, axis=-1, keepdims=True)
+        ea0 = tf.exp(a0)
+        ea1 = tf.exp(a1)
+        z0 = tf.reduce_sum(ea0, axis=-1, keepdims=True)
+        z1 = tf.reduce_sum(ea1, axis=-1, keepdims=True)
+        p0 = ea0 / z0
+        return tf.reduce_sum(p0 * (a0 - tf.log(z0) - a1 + tf.log(z1)), axis=-1)
+
+    def entropy(self):
+        a0 = self.logits - tf.reduce_max(self.logits, axis=-1, keepdims=True)
+        ea0 = tf.exp(a0)
+        z0 = tf.reduce_sum(ea0, axis=-1, keepdims=True)
+        p0 = ea0 / z0
+        return tf.reduce_sum(p0 * (tf.log(z0) - a0), axis=-1)
+
+    def sample(self):
+        u = tf.random_uniform(tf.shape(self.logits))
+        return tf.argmax(self.logits - tf.log(-tf.log(u)), axis=-1)
+
+    @classmethod
+    def fromflat(cls, flat):
+        return cls(flat)
+
+
+class MultiCategoricalPd(PD):
+    def __init__(self, nvec, flat):
+        self.flat = flat
+        self.categoricals = list(
+            map(CategoricalPd, tf.split(flat, nvec, axis=-1)))
+
+    def flatparam(self):
+        return self.flat
+
+    def mode(self):
+        return tf.cast(tf.stack([p.mode() for p in self.categoricals], axis=-1), tf.int32)
+
+    def neglogp(self, x):
+        return tf.add_n([p.neglogp(px) for p, px in zip(self.categoricals, tf.unstack(x, axis=-1))])
+
+    def kl(self, other):
+        return tf.add_n([p.kl(q) for p, q in zip(self.categoricals, other.categoricals)])
+
+    def entropy(self):
+        return tf.add_n([p.entropy() for p in self.categoricals])
+
+    def sample(self):
+        return tf.cast(tf.stack([p.sample() for p in self.categoricals], axis=-1), tf.int32)
+
+    @classmethod
+    def fromflat(cls, flat):
+        raise NotImplementedError
+
+
+class DiagGaussianPd(PD):
+    def __init__(self, flat):
+        self.flat = flat
+        mean, logstd = tf.split(
+            axis=len(flat.shape) - 1, num_or_size_splits=2, value=flat)
+        self.mean = mean
+        self.logstd = logstd
+        self.std = tf.exp(logstd)
+
+    def flatparam(self):
+        return self.flat
+
+    def mode(self):
+        return self.mean
+
+    def neglogp(self, x):
+        return 0.5 * tf.reduce_sum(tf.square((x - self.mean) / self.std), axis=-1) \
+            + 0.5 * np.log(2.0 * np.pi) * tf.to_float(tf.shape(x)[-1]) \
+            + tf.reduce_sum(self.logstd, axis=-1)
+
+    def kl(self, other):
+        assert isinstance(other, DiagGaussianPd)
+        return tf.reduce_sum(other.logstd - self.logstd + (tf.square(self.std) + tf.square(self.mean - other.mean)) / (2.0 * tf.square(other.std)) - 0.5, axis=-1)
+
+    def entropy(self):
+        return tf.reduce_sum(self.logstd + 0.5 * np.log(2.0 * np.pi * np.e), axis=-1)
+
+    def sample(self):
+        return self.mean + self.std * tf.random_normal(tf.shape(self.mean))
+
+    @classmethod
+    def fromflat(cls, flat):
+        return cls(flat)
+
+
+class BernoulliPd(PD):
+    def __init__(self, logits):
+        self.logits = logits
+        self.ps = tf.sigmoid(logits)
+
+    def flatparam(self):
+        return self.logits
+
+    def mode(self):
+        return tf.round(self.ps)
+
+    def neglogp(self, x):
+        return tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(logits=self.logits, labels=tf.to_float(x)), axis=-1)
+
+    def kl(self, other):
+        return tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(logits=other.logits, labels=self.ps), axis=-1) - tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(logits=self.logits, labels=self.ps), axis=-1)
+
+    def entropy(self):
+        return tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(logits=self.logits, labels=self.ps), axis=-1)
+
+    def sample(self):
+        u = tf.random_uniform(tf.shape(self.ps))
+        return tf.to_float(math_ops.less(u, self.ps))
+
+    @classmethod
+    def fromflat(cls, flat):
+        return cls(flat)
+
+
+class CategoricalPDType(PDType):
+    def __init__(self, ncat):
+        self.ncat = ncat
+
+    def pdclass(self):
+        return CategoricalPd
+
+    def param_shape(self):
+        return [self.ncat]
+
+    def sample_shape(self):
+        return []
+
+    def sample_dtype(self):
+        return tf.int32
+
+
+class MultiCategoricalPDType(PDType):
+    def __init__(self, nvec):
+        self.ncats = nvec
+
+    def pdclass(self):
+        return MultiCategoricalPd
+
+    def pdfromflat(self, flat):
+        return MultiCategoricalPd(self.ncats, flat)
+
+    def param_shape(self):
+        return [sum(self.ncats)]
+
+    def sample_shape(self):
+        return [len(self.ncats)]
+
+    def sample_dtype(self):
+        return tf.int32
+
+
+class DiagGaussianPDType(PDType):
+    def __init__(self, size):
+        self.size = size
+
+    def pdclass(self):
+        return DiagGaussianPd
+
+    def param_shape(self):
+        return [2 * self.size]
+
+    def sample_shape(self):
+        return [self.size]
+
+    def sample_dtype(self):
+        return tf.float32
+
+
+class BernoulliPDType(PDType):
+    def __init__(self, size):
+        self.size = size
+
+    def pdclass(self):
+        return BernoulliPd
+
+    def param_shape(self):
+        return [self.size]
+
+    def sample_shape(self):
+        return [self.size]
+
+    def sample_dtype(self):
+        return tf.int32
+
+
+def make_pdtype(body):
+    space = body.action_space
+    dim = body.action_dim
+    if space == 'Box':
+        return DiagGaussianPDType(dim)
+    elif space == 'Discrete':
+        return CategoricalPDType(dim)
+    elif space == 'MultiDiscrete':
+        return MultiCategoricalPDType(dim)
+    elif space == 'MultiBinary':
+        return BernoulliPDType(dim)
+    else:
+        raise NotImplementedError
