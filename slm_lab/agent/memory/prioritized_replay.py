5049260cffd9fe03c2071142a59b78a530a2e1bf
lgraesser <lhgraesser@gmail.com>
2018-03-18 11:24:43 -0600

    Prioritized replay memory

--- slm_lab/agent/memory/prioritized_replay.py
+++ slm_lab/agent/memory/prioritized_replay.py
@@ -4,6 +4,7 @@ from slm_lab.lib import util
 from slm_lab.lib.decorator import lab_api
 import numpy as np
 import pydash as _
+import random
 
 
 class PrioritizedReplay(Replay):
@@ -14,33 +15,52 @@ class PrioritizedReplay(Replay):
     '''
     def __init__(self, body):
         super(PrioritizedReplay, self).__init__(body)
+        self.e = self.body.agent.spec['memory']['e']
+        self.alpha = self.body.agent.spec['memory']['alpha']
 
     def reset(self):
         super(PrioritizedReplay, self).reset()
         self.tree = SumTree(self.max_size)
 
     @lab_api
-    def update(self, action, reward, state, done, priority):
+    def update(self, action, reward, state, done):
         '''Interface method to update memory'''
-        # TODO
-        pass
+        super(PrioritizedReplay, self).update(action, reward, state, done)
 
-    def add_experience(self, state, action, reward, next_state, done, priority=1):
-        '''Implementation for update() to add experience to memory, expanding the memory size if necessary'''
+    def add_experience(self, state, action, reward, next_state, done, error=100000):
+        '''Implementation for update() to add experience to memory, expanding the memory size if necessary.
+        All experiences are added with a high priority to increase the likelihood that they are sampled at least once.'''
+        priority = self.get_priority(error)
         super(PrioritizedReplay, self).add_experience(state, action, reward, next_state, done, priority)
-        # TODO
-        pass
+        self.tree.add(priority, self.head)
+
+    def get_priority(self, error):
+        '''Takes in the error of one or more examples and returns the proportional priority'''
+        p = torch.pow(torch.mul(error, self.e), self.alpha)
+        return p.data.numpy()
 
     def sample_idxs(self, batch_size):
-        '''Batch indices a sampled random uniformly'''
-        # TODO
-        pass
+        '''Samples batch_size indices from memory in proportional to their priority.
+        Implementation follows the approach in the paper "Prioritized Experience Replay", Schaul et al 2015" and is Jaromír Janisch's with minor adaptations. See memory_utils.py for the license and link to Jaromír's excellent blog'''
+        batch_idxs = []
+        segment = self.tree.total() / batch_size
 
-    def update_priorities(self, priorities):
+        for i in range(batch_size):
+            a = segment * i
+            b = segment * (i + 1)
+
+            s = random.uniform(a, b)
+            (_, p, idx) = self.tree.get(s)
+            batch_idxs.append(idx)
+
+        return batch_idx
+
+    def update_priorities(self, errors):
         '''
         Updates the priorities from the most recent batch
         Assumes the relevant batch indices are stored in self.batch_idxs
         '''
+        priorities = self.get_priority(errors)
         super(PrioritizedReplay, self).update_priorities(priorities)
-        # TODO
-        pass
+        for p, i in zip(priorities, batch_idxs):
+            self.tree.update(i, p)
