e879bfc07c7cb6c0455862bf2e869f5d172ea44f
lgraesser <lhgraesser@gmail.com>
2018-04-07 11:31:23 -0400

    Changing session data warning threshold

--- slm_lab/experiment/analysis.py
+++ slm_lab/experiment/analysis.py
@@ -54,8 +54,8 @@ def get_session_data(session):
     logger.debug(f'{session_data}')
     data_size_in_bytes = util.total_size(session_mdp_data)
     logger.info(f'Size of session data: {data_size_in_bytes / 1000000} MB')
-    if data_size_in_bytes / 1000000 > 10:
-        logger.warn(f'Session data > 10 MB')
+    if data_size_in_bytes / 1000000 > 25:
+        logger.warn(f'Session data > 25 MB')
     return session_mdp_data, session_data
 
 
