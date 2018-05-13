74b7e913ec6c0ad6e3aafc631c3c1642c42beade
kengz <kengzwl@gmail.com>
2018-05-13 12:29:10 -0700

    update and tested VanillaDQN with new API

--- slm_lab/agent/algorithm/dqn.py
+++ slm_lab/agent/algorithm/dqn.py
@@ -15,7 +15,8 @@ logger = logger.get_logger(__name__)
 
 
 class VanillaDQN(SARSA):
-    '''Implementation of a simple DQN algorithm.
+    '''
+    Implementation of a simple DQN algorithm.
     Algorithm:
         1. Collect some examples by acting in the environment and store them in a replay memory
         2. Every K steps sample N examples from replay memory
@@ -44,7 +45,8 @@ class VanillaDQN(SARSA):
 
     @lab_api
     def post_body_init(self):
-        '''Initializes the part of algorithm needing a body to exist first. A body is a part of an Agent. Agents may have 1 to k bodies. Bodies do the acting in environments, and contain:
+        '''
+        Initializes the part of algorithm needing a body to exist first. A body is a part of an Agent. Agents may have 1 to k bodies. Bodies do the acting in environments, and contain:
             - Memory (holding experiences obtained by acting in the environment)
             - State and action dimentions for an environment
             - Boolean var for if the action space is discrete
@@ -54,34 +56,18 @@ class VanillaDQN(SARSA):
     @lab_api
     def init_nets(self):
         '''Initialize the neural network used to learn the Q function from the spec'''
-        super(VanillaDQN, self).init_nets()
-        '''Guard againsts recurrent nets. Not supported with with DQN family of algorithms.'''
-        net_spec = self.agent_spec['net']
-        if net_spec['type'].find('Recurrent') != -1:
-            logger.warn(f'Recurrent networks not with DQN family of algorithms. Please select another network type''')
-            sys.exit()
-
-    def set_net_attributes(self):
-        '''Initializes additional parameters from the net spec. Called by init_nets'''
-        net_spec = self.agent_spec['net']
-        util.set_attr(self, net_spec, [
-            # how many examples to learn per training iteration
-            'batch_size',
-            'decay_lr_factor', 'decay_lr_frequency', 'decay_lr_min_timestep', 'gpu'
-        ])
-        if not hasattr(self, 'gpu'):
-            self.net.gpu = False
+        self.body = self.agent.nanflat_body_a[0]  # single-body algo
+        if 'Recurrent' in self.net_spec['type']:
+            raise ValueError('Recurrent networks not with DQN family of algorithms.')
+        NetClass = getattr(net, self.net_spec['type'])
+        self.net = NetClass(self, self.body)
         logger.info(f'Training on gpu: {self.net.gpu}')
 
     @lab_api
     def init_algorithm_params(self):
-        '''Initialize other algorithm parameters'''
-        super(VanillaDQN, self).init_algorithm_params()
-
-    def set_other_algorithm_attributes(self):
-        '''Initializes additional parameters from the algorithm spec. Called by init_algorithm_params'''
-        algorithm_spec = self.agent_spec['algorithm']
-        util.set_attr(self, algorithm_spec, [
+        util.set_attr(self, self.algorithm_spec, [
+            'action_policy',
+            'action_policy_update',
             # explore_var is epsilon, tau or etc. depending on the action policy
             # these control the trade off between exploration and exploitaton
             'explore_var_start', 'explore_var_end', 'explore_anneal_epi',
@@ -91,6 +77,7 @@ class VanillaDQN(SARSA):
             'training_iters_per_batch',  # how many times to train each batch
             'training_min_timestep',  # how long before starting training
         ])
+        super(VanillaDQN, self).init_algorithm_params()
 
     def compute_q_target_values(self, batch):
         '''Computes the target Q values for a batch of experiences'''
@@ -105,34 +92,31 @@ class VanillaDQN(SARSA):
         logger.debug2(f'Q next_states max {q_next_st_max.size()}')
         # Compute q_targets using reward and estimated best Q value from the next state if there is one
         # Make future reward 0 if the current state is done
-        q_targets_max = batch['rewards'].data + self.gamma * \
-            torch.mul((1 - batch['dones'].data), q_next_st_max)
+        q_targets_max = batch['rewards'].data + self.gamma * torch.mul((1 - batch['dones'].data), q_next_st_max)
         logger.debug2(f'Q targets max: {q_targets_max.size()}')
         # We only want to train the network for the action selected in the current state
         # For all other actions we set the q_target = q_sts so that the loss for these actions is 0
-        q_targets = torch.mul(q_targets_max, batch['actions'].data) + \
-            torch.mul(q_sts, (1 - batch['actions'].data))
+        q_targets = torch.mul(q_targets_max, batch['actions'].data) + torch.mul(q_sts, (1 - batch['actions'].data))
         logger.debug2(f'Q targets: {q_targets.size()}')
         return q_targets
 
     @lab_api
     def sample(self):
-        '''Samples a batch from memory of size self.batch_size'''
-        batches = [body.memory.sample(self.batch_size)
-                   for body in self.agent.nanflat_body_a]
+        '''Samples a batch from memory of size self.memory.batch_size'''
+        # TODO internalize batch_size to memory
+        batches = [body.memory.sample() for body in self.agent.nanflat_body_a]
         batch = util.concat_dict(batches)
         util.to_torch_batch(batch, self.net.gpu)
         return batch
 
     @lab_api
     def train(self):
-        '''Completes one training step for the agent if it is time to train.
-           i.e. the environment timestep is greater than the minimum training
-           timestep and a multiple of the training_frequency.
-           Each training step consists of sampling n batches from the agent's memory.
-              For each of the batches, the target Q values (q_targets) are computed and
-              a single training step is taken k times
-           Otherwise this function does nothing.
+        '''
+        Completes one training step for the agent if it is time to train.
+        i.e. the environment timestep is greater than the minimum training timestep and a multiple of the training_frequency.
+        Each training step consists of sampling n batches from the agent's memory.
+        For each of the batches, the target Q values (q_targets) are computed and a single training step is taken k times
+        Otherwise this function does nothing.
         '''
         t = util.s_get(self, 'aeb_space.clock').get('total_t')
         if (t > self.training_min_timestep and t % self.training_frequency == 0):
@@ -202,54 +186,26 @@ class DQNBase(VanillaDQN):
     @lab_api
     def init_nets(self):
         '''Initialize networks'''
-        body = self.agent.nanflat_body_a[0]  # single-body algo
-        self.state_dim = body.state_dim
-        self.action_dim = body.action_dim
-        net_spec = self.agent_spec['net']
-        logger.debug3(f'State dim: {self.state_dim}')
-        net_kwargs = util.compact_dict(dict(
-            hid_layers_activation=ps.get(net_spec, 'hid_layers_activation'),
-            optim_spec=ps.get(net_spec, 'optim'),
-            loss_spec=ps.get(net_spec, 'loss'),
-            clamp_grad=ps.get(net_spec, 'clamp_grad'),
-            clamp_grad_val=ps.get(net_spec, 'clamp_grad_val'),
-            gpu=ps.get(net_spec, 'gpu'),
-            decay_lr_factor=ps.get(net_spec, 'decay_lr_factor'),
-        ))
-        ''' Make adjustments for Atari mode '''
-        if self.agent_spec['memory']['name'].find('Atari') != -1:
-            self.state_dim = (84, 84, 4)
-            logger.debug3(f'State dim: {self.state_dim}')
-            net_kwargs = util.compact_dict(dict(
-                hid_layers_activation=ps.get(net_spec, 'hid_layers_activation'),
-                optim_spec=ps.get(net_spec, 'optim'),
-                loss_spec=ps.get(net_spec, 'loss'),
-                clamp_grad=ps.get(net_spec, 'clamp_grad'),
-                clamp_grad_val=ps.get(net_spec, 'clamp_grad_val'),
-                batch_norm=ps.get(net_spec, 'batch_norm'),
-                gpu=ps.get(net_spec, 'gpu'),
-                decay_lr_factor=ps.get(net_spec, 'decay_lr_factor'),
-            ))
-        elif self.agent_spec['memory']['name'].find('Stack') != -1:
-            ''' Make adjustments for StackedReplay memory '''
-            if net_spec['type'].find('MLP') == -1:
-                logger.warn(f'StackedReplay should only be used with MLPs, to stack states with ConvNets use Atari memory. It is not necessary to stack states with RNNs''')
-                sys.exit()
-            self.state_dim = self.state_dim * self.agent_spec['memory']['seq_len']
-            logger.debug3(f'State dim: {self.state_dim}')
-            net_kwargs = util.compact_dict(dict(
-                hid_layers_activation=ps.get(net_spec, 'hid_layers_activation'),
-                optim_spec=ps.get(net_spec, 'optim'),
-                loss_spec=ps.get(net_spec, 'loss'),
-                clamp_grad=ps.get(net_spec, 'clamp_grad'),
-                clamp_grad_val=ps.get(net_spec, 'clamp_grad_val'),
-                batch_norm=ps.get(net_spec, 'batch_norm'),
-                gpu=ps.get(net_spec, 'gpu'),
-                decay_lr_factor=ps.get(net_spec, 'decay_lr_factor'),
-            ))
-        if net_spec['type'].find('Recurrent') != -1:
-            logger.warn(f'Recurrent networks not supported with DQN family of algorithms. Please select another network type''')
-            sys.exit()
+        self.body = self.agent.nanflat_body_a[0]  # single-body algo
+        if 'Recurrent' in self.net_spec['type']:
+            raise ValueError('Recurrent networks not with DQN family of algorithms.')
+
+        memory_name = self.memory_spec['name']
+        if 'Atari' in memory_name:
+            # Make adjustments for Atari mode
+            # TODO should be auto-set from preprocessor
+            self.body.state_dim = (84, 84, 4)
+        elif 'Stack' in memory_name:
+            # Make adjustments for StackedReplay memory
+            if 'MLP' not in self.net_spec['type']:
+                raise ValueError('StackedReplay should only be used with MLPs, to stack states with ConvNets use Atari memory. It is not necessary to stack states with RNNs')
+            self.body.state_dim = self.body.state_dim * self.memory_spec['seq_len']
+        logger.debug3(f'State dim: {self.body.state_dim}')
+        # TODO batch_norm in net_spec?
+        NetClass = getattr(net, self.net_spec['type'])
+        self.net = NetClass(self, self.body)
+        logger.info(f'Training on gpu: {self.net.gpu}')
+
         self.net = getattr(net, net_spec['type'])(
             self.state_dim, net_spec['hid_layers'], self.action_dim, **net_kwargs)
         self.target_net = getattr(net, net_spec['type'])(
@@ -257,7 +213,6 @@ class DQNBase(VanillaDQN):
         self.online_net = self.target_net
         self.eval_net = self.target_net
         util.set_attr(self, net_spec, [
-            'batch_size',
             'decay_lr_factor', 'decay_lr_frequency', 'decay_lr_min_timestep', 'gpu'
         ])
         if not hasattr(self, 'gpu'):
@@ -279,21 +234,19 @@ class DQNBase(VanillaDQN):
         # Evaluate the action selection using the eval net
         q_next_sts = self.eval_net.wrap_eval(batch['next_states'])
         logger.debug2(f'Q next_states: {q_next_sts.size()}')
-        idx = torch.from_numpy(np.array(list(range(self.batch_size))))
+        idx = torch.from_numpy(np.array(list(range(self.memory.batch_size))))
         if torch.cuda.is_available() and self.net.gpu:
             idx = idx.cuda()
         q_next_st_maxs = q_next_sts[idx, q_next_acts]
         q_next_st_maxs.unsqueeze_(1)
         logger.debug2(f'Q next_states max {q_next_st_maxs.size()}')
         # Compute final q_target using reward and estimated best Q value from the next state if there is one. Make next state Q value 0 if the current state is done
-        q_targets_max = batch['rewards'].data + self.gamma * \
-            torch.mul((1 - batch['dones'].data), q_next_st_maxs)
+        q_targets_max = batch['rewards'].data + self.gamma * torch.mul((1 - batch['dones'].data), q_next_st_maxs)
         logger.debug2(f'Q targets max: {q_targets_max.size()}')
         # We only want to train the network for the action selected
         # For all other actions we set the q_target = q_sts
         # So that the loss for these actions is 0
-        q_targets = torch.mul(q_targets_max, batch['actions'].data) + \
-            torch.mul(q_sts, (1 - batch['actions'].data))
+        q_targets = torch.mul(q_targets_max, batch['actions'].data) + torch.mul(q_sts, (1 - batch['actions'].data))
         logger.debug2(f'Q targets: {q_targets.size()}')
         return q_targets
 
@@ -308,8 +261,7 @@ class DQNBase(VanillaDQN):
                 self.eval_net = self.target_net
         elif self.update_type == 'polyak':
             logger.debug('Updating net by averaging')
-            avg_params = self.polyak_weight * net_util.flatten_params(self.target_net) + \
-                (1 - self.polyak_weight) * net_util.flatten_params(self.net)
+            avg_params = self.polyak_weight * net_util.flatten_params(self.target_net) + (1 - self.polyak_weight) * net_util.flatten_params(self.net)
             self.target_net = net_util.load_params(self.target_net, avg_params)
             self.online_net = self.target_net
             self.eval_net = self.target_net
@@ -357,7 +309,10 @@ class DoubleDQN(DQN):
 
 
 class MultitaskDQN(DQN):
-    '''Simplest Multi-task DQN implementation. States and action dimensions are concatenated, and a single shared network is reponsible for processing concatenated states, and generating one action per environment from a single output layer.'''
+    '''
+    Simplest Multi-task DQN implementation.
+    States and action dimensions are concatenated, and a single shared network is reponsible for processing concatenated states, and generating one action per environment from a single output layer.
+    '''
 
     @lab_api
     def init_nets(self):
@@ -385,7 +340,6 @@ class MultitaskDQN(DQN):
         self.online_net = self.target_net
         self.eval_net = self.target_net
         util.set_attr(self, net_spec, [
-            'batch_size',
             'decay_lr_factor', 'decay_lr_frequency', 'decay_lr_min_timestep',
             'update_type', 'update_frequency', 'polyak_weight', 'gpu'
         ])
@@ -396,7 +350,7 @@ class MultitaskDQN(DQN):
     @lab_api
     def sample(self):
         # NOTE the purpose of multi-body is to parallelize and get more batch_sizes
-        batches = [body.memory.sample(self.batch_size)
+        batches = [body.memory.sample()
                    for body in self.agent.nanflat_body_a]
         # Package data into pytorch variables
         for batch_b in batches:
@@ -438,7 +392,7 @@ class MultitaskDQN(DQN):
         q_next_sts = self.eval_net.wrap_eval(batch['next_states'])
         logger.debug2(f'Q next_states: {q_next_sts.size()}')
         logger.debug3(f'Q next_states: {q_next_sts}')
-        idx = torch.from_numpy(np.array(list(range(self.batch_size))))
+        idx = torch.from_numpy(np.array(list(range(self.memory.batch_size))))
         if torch.cuda.is_available() and self.net.gpu:
             idx = idx.cuda()
         q_next_st_maxs = []
@@ -473,8 +427,7 @@ class MultitaskDQN(DQN):
         # We only want to train the network for the action selected
         # For all other actions we set the q_target = q_sts
         # So that the loss for these actions is 0
-        q_targets = torch.mul(q_targets_maxs, combined_actions.data) + \
-            torch.mul(q_sts, (1 - combined_actions.data))
+        q_targets = torch.mul(q_targets_maxs, combined_actions.data) + torch.mul(q_sts, (1 - combined_actions.data))
         logger.debug2(f'Q targets: {q_targets.size()}')
         logger.debug3(f'Q targets: {q_targets}')
         return q_targets
@@ -525,7 +478,6 @@ class MultiHeadDQN(MultitaskDQN):
         self.online_net = self.target_net
         self.eval_net = self.target_net
         util.set_attr(self, net_spec, [
-            'batch_size',
             'decay_lr_factor', 'decay_lr_frequency', 'decay_lr_min_timestep',
             'update_type', 'update_frequency', 'polyak_weight', 'gpu'
         ])
@@ -536,7 +488,7 @@ class MultiHeadDQN(MultitaskDQN):
     @lab_api
     def sample(self):
         '''Samples one batch per environment'''
-        batches = [body.memory.sample(self.batch_size)
+        batches = [body.memory.sample()
                    for body in self.agent.nanflat_body_a]
         # Package data into pytorch variables
         for batch_b in batches:
@@ -564,7 +516,7 @@ class MultiHeadDQN(MultitaskDQN):
         # Select q_next_st_maxs based on action selected in q_next_acts
         q_next_sts = self.eval_net.wrap_eval(batch['next_states'])
         logger.debug3(f'Q next_states: {q_next_sts}')
-        idx = torch.from_numpy(np.array(list(range(self.batch_size))))
+        idx = torch.from_numpy(np.array(list(range(self.memory.batch_size))))
         if torch.cuda.is_available() and self.net.gpu:
             idx = idx.cuda()
         q_next_st_maxs = []
@@ -578,8 +530,7 @@ class MultiHeadDQN(MultitaskDQN):
         # Make future reward 0 if the current state is done
         q_targets_maxs = []
         for b, batch_b in enumerate(batches):
-            q_targets_max_b = batch_b['rewards'].data + self.gamma * \
-                torch.mul((1 - batch_b['dones'].data), q_next_st_maxs[b])
+            q_targets_max_b = batch_b['rewards'].data + self.gamma * torch.mul((1 - batch_b['dones'].data), q_next_st_maxs[b])
             q_targets_maxs.append(q_targets_max_b)
             logger.debug2(
                 f'Batch {b}, Q targets max: {q_targets_max_b.size()}')
@@ -588,21 +539,19 @@ class MultiHeadDQN(MultitaskDQN):
         # So that the loss for these actions is 0
         q_targets = []
         for b, batch_b in enumerate(batches):
-            q_targets_b = torch.mul(q_targets_maxs[b], batch_b['actions'].data) + \
-                torch.mul(q_sts[b], (1 - batch_b['actions'].data))
+            q_targets_b = torch.mul(q_targets_maxs[b], batch_b['actions'].data) + torch.mul(q_sts[b], (1 - batch_b['actions'].data))
             q_targets.append(q_targets_b)
             logger.debug2(f'Batch {b}, Q targets: {q_targets_b.size()}')
         return q_targets
 
     @lab_api
     def train(self):
-        '''Completes one training step for the agent if it is time to train.
-           i.e. the environment timestep is greater than the minimum training
-           timestep and a multiple of the training_frequency.
-           Each training step consists of sampling n batches from the agent's memory.
-              For each of the batches, the target Q values (q_targets) are computed and
-              a single training step is taken k times
-           Otherwise this function does nothing.
+        '''
+        Completes one training step for the agent if it is time to train.
+        i.e. the environment timestep is greater than the minimum training timestep and a multiple of the training_frequency.
+        Each training step consists of sampling n batches from the agent's memory.
+        For each of the batches, the target Q values (q_targets) are computed and a single training step is taken k times
+        Otherwise this function does nothing.
         '''
         t = util.s_get(self, 'aeb_space.clock').get('total_t')
         if (t > self.training_min_timestep and t % self.training_frequency == 0):
