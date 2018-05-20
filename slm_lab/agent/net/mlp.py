2ad872304d75f8b8e31e1f484c5b6455a94aa8e5
kengz <kengzwl@gmail.com>
2018-05-20 16:28:48 -0700

    rewrite mlp indepdendent of body

--- slm_lab/agent/net/mlp.py
+++ slm_lab/agent/net/mlp.py
@@ -14,7 +14,7 @@ class MLPNet(Net, nn.Module):
     Class for generating arbitrary sized feedforward neural network
     '''
 
-    def __init__(self, net_spec, algorithm, body):
+    def __init__(self, net_spec, algorithm, in_dim, out_dim):
         '''
         net_spec:
         hid_layers: list containing dimensions of the hidden layers
@@ -33,7 +33,7 @@ class MLPNet(Net, nn.Module):
         gpu: whether to train using a GPU. Note this will only work if a GPU is available, othewise setting gpu=True does nothing
         '''
         nn.Module.__init__(self)
-        super(MLPNet, self).__init__(net_spec, algorithm, body)
+        super(MLPNet, self).__init__(net_spec, algorithm, in_dim, out_dim)
         # set default
         util.set_attr(self, dict(
             optim_spec={'name': 'Adam'},
@@ -63,15 +63,15 @@ class MLPNet(Net, nn.Module):
             'gpu',
         ])
 
-        dims = [self.body.state_dim] + self.hid_layers
+        dims = [self.in_dim] + self.hid_layers
         self.model = net_util.build_sequential(dims, self.hid_layers_activation)
         # add last layer with no activation
-        self.model.add_module(str(len(self.model)), nn.Linear(dims[-1], self.body.action_dim))
+        self.model.add_module(str(len(self.model)), nn.Linear(dims[-1], self.out_dim))
+
         net_util.init_layers(self.modules())
         if torch.cuda.is_available() and self.gpu:
             for module in self.modules():
                 module.cuda()
-
         self.loss_fn = net_util.get_loss_fn(self, self.loss_spec)
         self.optim = net_util.get_optim(self, self.optim_spec)
         logger.info(f'loss fn: {self.loss_fn}')
@@ -120,9 +120,9 @@ class MLPHeterogenousTails(MLPNet):
     Class for generating arbitrary sized feedforward neural network, with a heterogenous set of output tails that may correspond to different values. For example, the mean or std deviation of a continous policy, the state-value estimate, or the logits of a categorical action distribution
     '''
 
-    def __init__(self, net_spec, algorithm, body):
+    def __init__(self, net_spec, algorithm, in_dim, out_dim):
         nn.Module.__init__(self)
-        Net.__init__(self, net_spec, algorithm, body)
+        Net.__init__(self, net_spec, algorithm, in_dim, out_dim)
         # set default
         util.set_attr(self, dict(
             optim_spec={'name': 'Adam'},
@@ -152,15 +152,15 @@ class MLPHeterogenousTails(MLPNet):
             'gpu',
         ])
 
-        dims = [self.body.state_dim] + self.hid_layers
+        dims = [self.in_dim] + self.hid_layers
         self.model_body = net_util.build_sequential(dims, self.hid_layers_activation)
-        # multi-tail output layer
-        self.model_tails = nn.ModuleList([nn.Linear(dims[-1], out_d) for out_d in self.body.action_dim])
+        # multi-tail output layer with mean and std
+        self.model_tails = nn.ModuleList([nn.Linear(dims[-1], out_d) for out_d in out_dim])
+
         net_util.init_layers(self.modules())
         if torch.cuda.is_available() and self.gpu:
             for module in self.modules():
                 module.cuda()
-
         self.loss_fn = net_util.get_loss_fn(self, self.loss_spec)
         self.optim = net_util.get_optim(self, self.optim_spec)
         logger.info(f'loss fn: {self.loss_fn}')
@@ -188,7 +188,7 @@ class MultiMLPNet(Net, nn.Module):
     Class for generating arbitrary sized feedforward neural network with multiple state and action heads, and a single shared body.
     '''
 
-    def __init__(self, net_spec, algorithm, body_list):
+    def __init__(self, net_spec, algorithm, in_dim, out_dim):
         '''
         Multi state processing heads, single shared body, and multi action tails.
         There is one state and action head per body/environment
@@ -213,7 +213,7 @@ class MultiMLPNet(Net, nn.Module):
            env 1 action      env 2 action
         '''
         nn.Module.__init__(self)
-        super(MultiMLPNet, self).__init__(net_spec, algorithm, body_list)
+        super(MultiMLPNet, self).__init__(net_spec, algorithm, in_dim, out_dim)
         # set default
         util.set_attr(self, dict(
             optim_spec={'name': 'Adam'},
@@ -243,47 +243,49 @@ class MultiMLPNet(Net, nn.Module):
             'gpu',
         ])
         assert len(self.hid_layers) == 3, 'Your hidden layers must specify [*heads], [body], [*tails]. If not, use MLPHeterogenousTails'
+        assert isinstance(self.in_dim, list), 'Hydra network needs in_dim as list'
+        assert isinstance(self.out_dim, list), 'Hydra network needs out_dim as list'
         self.head_hid_layers = self.hid_layers[0]
         self.body_hid_layers = self.hid_layers[1]
         self.tail_hid_layers = self.hid_layers[2]
         if len(self.head_hid_layers) == 1:
-            self.head_hid_layers = self.head_hid_layers * len(body_list)
+            self.head_hid_layers = self.head_hid_layers * len(self.in_dim)
         if len(self.tail_hid_layers) == 1:
-            self.tail_hid_layers = self.tail_hid_layers * len(body_list)
+            self.tail_hid_layers = self.tail_hid_layers * len(self.out_dim)
 
-        self.model_heads = self.build_model_heads(body_list)
+        self.model_heads = self.build_model_heads(in_dim)
         heads_out_dim = np.sum([head[-1].out_features for head in self.model_heads])
         dims = [heads_out_dim] + self.body_hid_layers
         self.model_body = net_util.build_sequential(dims, self.hid_layers_activation)
-        self.model_tails = self.build_model_tails(body_list)
+        self.model_tails = self.build_model_tails(out_dim)
+
         net_util.init_layers(self.modules())
         if torch.cuda.is_available() and self.gpu:
             for module in self.modules():
                 module.cuda()
-
         self.loss_fn = net_util.get_loss_fn(self, self.loss_spec)
         self.optim = net_util.get_optim(self, self.optim_spec)
         logger.info(f'loss fn: {self.loss_fn}')
         logger.info(f'optimizer: {self.optim}')
 
-    def build_model_heads(self, body_list):
+    def build_model_heads(self, in_dim):
         '''Build each model_head. These are stored as Sequential models in model_heads'''
-        assert len(self.head_hid_layers) == len(body_list), 'Hydra head hid_params inconsistent with number of bodies'
+        assert len(self.head_hid_layers) == len(in_dim), 'Hydra head hid_params inconsistent with number in dims'
         model_heads = nn.ModuleList()
-        for body, hid_layers in zip(body_list, self.head_hid_layers):
-            dims = [body.state_dim] + hid_layers
+        for in_d, hid_layers in zip(in_dim, self.head_hid_layers):
+            dims = [in_d] + hid_layers
             model_head = net_util.build_sequential(dims, self.hid_layers_activation)
             model_heads.append(model_head)
         return model_heads
 
-    def build_model_tails(self, body_list):
+    def build_model_tails(self, out_dim):
         '''Build each model_tail. These are stored as Sequential models in model_tails'''
-        assert len(self.tail_hid_layers) == len(body_list), 'Hydra tail hid_params inconsistent with number of bodies'
+        assert len(self.tail_hid_layers) == len(out_dim), 'Hydra tail hid_params inconsistent with number out dims'
         model_tails = nn.ModuleList()
-        for body, hid_layers in zip(body_list, self.tail_hid_layers):
+        for out_d, hid_layers in zip(out_dim, self.tail_hid_layers):
             dims = hid_layers
             model_tail = net_util.build_sequential(dims, self.hid_layers_activation)
-            model_tail.add_module(str(len(model_tail)), nn.Linear(dims[-1], body.action_dim))
+            model_tail.add_module(str(len(model_tail)), nn.Linear(dims[-1], out_d))
             model_tails.append(model_tail)
         return model_tails
 
