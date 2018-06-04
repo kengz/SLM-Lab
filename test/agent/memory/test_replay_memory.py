f9882d1c32325f1774557d849703273a36a5eb63
kengz <kengzwl@gmail.com>
2018-06-04 00:31:53 -0700

    fix memory and trial demo tests

--- test/agent/memory/test_replay_memory.py
+++ test/agent/memory/test_replay_memory.py
@@ -19,10 +19,10 @@ class TestMemory:
         assert memory.true_size == 0
         assert memory.states.shape == (memory.max_size, memory.body.state_dim)
         assert memory.actions.shape == (memory.max_size, memory.body.action_dim)
-        assert memory.rewards.shape == (memory.max_size, 1)
+        assert memory.rewards.shape == (memory.max_size,)
         assert memory.next_states.shape == (memory.max_size, memory.body.state_dim)
-        assert memory.dones.shape == (memory.max_size, 1)
-        assert memory.priorities.shape == (memory.max_size, 1)
+        assert memory.dones.shape == (memory.max_size,)
+        assert memory.priorities.shape == (memory.max_size,)
 
     def test_add_experience(self, test_memory):
         '''Adds an experience to the memory. Checks that memory size = 1, and checks that the experience values are equal to the experience added'''
@@ -65,10 +65,10 @@ class TestMemory:
         batch = memory.sample()
         assert batch['states'].shape == (batch_size, memory.body.state_dim)
         assert batch['actions'].shape == (batch_size, memory.body.action_dim)
-        assert batch['rewards'].shape == (batch_size, 1)
+        assert batch['rewards'].shape == (batch_size,)
         assert batch['next_states'].shape == (batch_size, memory.body.state_dim)
-        assert batch['dones'].shape == (batch_size, 1)
-        assert batch['priorities'].shape == (batch_size, 1)
+        assert batch['dones'].shape == (batch_size,)
+        assert batch['priorities'].shape == (batch_size,)
 
     @flaky(max_runs=10)
     def test_sample_changes(self, test_memory):
