b4732e1fdb7fd39bdd063684ec30ccc87383846d
kengz <kengzwl@gmail.com>
2018-06-16 16:37:15 -0700

    rename file to prioritized

--- slm_lab/agent/memory/__init__.py
+++ slm_lab/agent/memory/__init__.py
@@ -6,4 +6,4 @@ Contains different ways of storing an agents experiences and sampling from them
 # expose all the classes
 from .replay import *
 from .onpolicy import *
-from .prioritizedreplay import *
+from .prioritized import *
