bcf2fdc0a9320b9309323f09906b024b62e51263
kengz <kengzwl@gmail.com>
2018-06-20 08:15:25 -0700

    add loss retain_graph arg

--- slm_lab/agent/net/recurrent.py
+++ slm_lab/agent/net/recurrent.py
@@ -157,7 +157,7 @@ class RecurrentNet(Net, nn.Module):
         else:
             return outs
 
-    def training_step(self, x=None, y=None, loss=None):
+    def training_step(self, x=None, y=None, loss=None, retain_graph=False):
         '''Takes a single training step: one forward and one backwards pass'''
         self.train()
         self.zero_grad()
@@ -166,7 +166,7 @@ class RecurrentNet(Net, nn.Module):
             out = self(x)
             loss = self.loss_fn(out, y)
         assert not torch.isnan(loss).any()
-        loss.backward()
+        loss.backward(retain_graph=retain_graph)
         if self.clip_grad:
             logger.debug(f'Clipping gradient')
             torch.nn.utils.clip_grad_norm_(self.parameters(), self.clip_grad_val)
