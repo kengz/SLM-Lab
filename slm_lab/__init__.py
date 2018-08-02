9771effb8dd4110abf0705fd327816fe8c747587
kengz <kengzwl@gmail.com>
2018-08-01 22:42:12 -0700

    safe-set ROOT_DIR

--- slm_lab/__init__.py
+++ slm_lab/__init__.py
@@ -6,7 +6,7 @@ CONFIG_NAME_MAP = {
     'test': 'example_default',
     'development': 'default',
 }
-ROOT_DIR = os.getcwd()
+ROOT_DIR = os.path.normpath(os.path.join(os.path.dirname(__file__), '..'))
 
 config_name = CONFIG_NAME_MAP.get(os.environ['PY_ENV'])
 with open(os.path.join(ROOT_DIR, 'config', f'{config_name}.json')) as f:
