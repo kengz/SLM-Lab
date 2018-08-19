2b72690ecc7fc9ee80a21322200392c69838aeb6
kengz <kengzwl@gmail.com>
2018-08-19 10:57:20 -0700

    fix ac global get net typo

--- slm_lab/agent/algorithm/actor_critic.py
+++ slm_lab/agent/algorithm/actor_critic.py
@@ -275,7 +275,7 @@ class ActorCritic(Reinforce):
             with torch.no_grad():
                 _advs, v_targets = self.calc_advs_v_targets(batch)
             val_loss = self.calc_val_loss(batch, v_targets)
-            self.critic.training_step(loss=val_loss, global_net=self.global_nets.get('net'))
+            self.critic.training_step(loss=val_loss, global_net=self.global_nets.get('critic'))
             total_val_loss += val_loss.cpu()
         val_loss = total_val_loss / self.training_epoch
         return val_loss
