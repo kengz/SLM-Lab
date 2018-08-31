4c6eb15fd27c6b53c04f4b803e335c0befb157c3
kengz <kengzwl@gmail.com>
2018-08-30 21:57:01 -0700

    make sarsa singleton

--- slm_lab/agent/algorithm/sarsa.py
+++ slm_lab/agent/algorithm/sarsa.py
@@ -40,19 +40,6 @@ class SARSA(Algorithm):
     '''
 
     @lab_api
-    def post_body_init(self):
-        '''
-        Initializes the part of algorithm needing a body to exist first. A body is a part of an Agent. Agents may have 1 to k bodies. Bodies do the acting in environments, and contain:
-            - Memory (holding experiences obtained by acting in the environment)
-            - State and action dimensions for an environment
-            - Boolean var for if the action space is discrete
-        '''
-        self.body = self.agent.nanflat_body_a[0]  # single-body algo
-        self.init_algorithm_params()
-        self.init_nets()
-        logger.info(util.self_desc(self))
-
-    @lab_api
     def init_algorithm_params(self):
         '''Initialize other algorithm parameters.'''
         # set default
@@ -79,8 +66,7 @@ class SARSA(Algorithm):
         self.to_train = 0
         self.action_policy = getattr(policy_util, self.action_policy)
         self.action_policy_update = getattr(policy_util, self.action_policy_update)
-        for body in self.agent.nanflat_body_a:
-            body.explore_var = self.explore_var_start
+        self.body.explore_var = self.explore_var_start
 
     @lab_api
     def init_nets(self):
@@ -110,8 +96,9 @@ class SARSA(Algorithm):
         return pdparam
 
     @lab_api
-    def body_act(self, body, state):
+    def act(self, state):
         '''Note, SARSA is discrete-only'''
+        body = self.body
         action, action_pd = self.action_policy(state, self, body)
         # sum for single and multi-action
         body.entropies.append(action_pd.entropy().sum(dim=0))
@@ -143,18 +130,14 @@ class SARSA(Algorithm):
     @lab_api
     def sample(self):
         '''Samples a batch from memory'''
-        batches = []
-        for body in self.agent.nanflat_body_a:
-            body_batch = body.memory.sample()
-            # one-hot actions to calc q_targets
-            if body.is_discrete:
-                body_batch['one_hot_actions'] = util.to_one_hot(body_batch['actions'], body.action_space.high)
-            batches.append(body_batch)
-        batch = util.concat_batches(batches)
+        batch = self.body.memory.sample()
+        # one-hot actions to calc q_targets
+        if self.body.is_discrete:
+            batch['one_hot_actions'] = util.to_one_hot(batch['actions'], self.body.action_space.high)
         # this is safe for next_action at done since the calculated act_next_q_preds will be multiplied by (1 - batch['dones'])
         batch['next_actions'] = np.zeros_like(batch['actions'])
         batch['next_actions'][:-1] = batch['actions'][1:]
-        batch = util.to_torch_batch(batch, self.net.gpu)
+        batch = util.to_torch_batch(batch, self.net.gpu, self.body.memory.is_episodic)
         return batch
 
     @lab_api
@@ -182,10 +165,8 @@ class SARSA(Algorithm):
     @lab_api
     def update(self):
         '''Update the agent after training'''
-        space_clock = util.s_get(self, 'aeb_space.clock')
         for net_name in self.net_names:
             net = getattr(self, net_name)
-            net.update_lr(space_clock)
-        explore_vars = [self.action_policy_update(self, body) for body in self.agent.nanflat_body_a]
-        explore_var_a = self.nanflat_to_data_a('explore_var', explore_vars)
-        return explore_var_a
+            net.update_lr(self.body.env.clock)
+        explore_var = self.action_policy_update(self, self.body)
+        return explore_var
