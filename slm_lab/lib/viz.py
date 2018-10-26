0c9949f52dac98f4a4655f2191477c370f186411
Wah Loon Keng <kengzwl@gmail.com>
2018-10-26 09:03:09 -0700

    Plotly update (#221)
    * hydradqn solves cartpole + 2dball
    
    * add to search
    
    * lower epi search
    
    * new search spec
    
    * bump plotly
    
    * add reward scale to env spec
    
    * use stable pip install plotly
    
    * update plotly methods to v3
    
    * remove q-learning
    
    * update plotly figure extend
    
    * try xvfb wrapper
    
    * hack viz
    
    * use process-safe xvfb caller
    
    * add xvfbwrapper to dep

--- slm_lab/lib/viz.py
+++ slm_lab/lib/viz.py
@@ -8,20 +8,25 @@ from plotly import (
     tools,
 )
 from slm_lab.lib import logger, util
-from subprocess import Popen, DEVNULL
+from xvfbwrapper import Xvfb
 import colorlover as cl
 import math
 import os
 import plotly
+import plotly.io as pio
 import pydash as ps
 import sys
-import ujson as json
 
 PLOT_FILEDIR = util.smart_path('data')
 os.makedirs(PLOT_FILEDIR, exist_ok=True)
 if util.is_jupyter():
     py.init_notebook_mode(connected=True)
 logger = logger.get_logger(__name__)
+
+# launch xvfb for Plotly if not on MacOSX
+if sys.platform != 'darwin':
+    vdisplay = Xvfb()
+    vdisplay.start()
 
 
 def create_label(
@@ -56,7 +61,7 @@ def create_layout(
         yaxis=dict(rangemode='tozero', title=y_title),
         xaxis=dict(type=x_type, title=x_title),
         width=width, height=height,
-        margin=go.Margin(l=60, r=60, t=60, b=60),
+        margin=go.layout.Margin(l=60, r=60, t=60, b=60),
     )
     layout.update(layout_kwargs)
     return layout
@@ -288,18 +293,9 @@ def save_image(figure, filepath=None):
     if filepath is None:
         filepath = f'{PLOT_FILEDIR}/{ps.get(figure, "layout.title")}.png'
     filepath = util.smart_path(filepath)
-    dirname, filename = os.path.split(filepath)
     try:
-        cmd = f'orca graph -o {filename} \'{json.dumps(figure)}\''
-        if 'linux' in sys.platform:
-            cmd = 'xvfb-run -a -s "-screen 0 1400x900x24" -- ' + cmd
-        proc = Popen(cmd, cwd=dirname, shell=True, stderr=DEVNULL, stdout=DEVNULL, close_fds=True)
-        try:
-            outs, errs = proc.communicate(timeout=20)
-        except TimeoutExpired:
-            proc.kill()
-            outs, errs = proc.communicate()
-        logger.info(f'Graph saved to {dirname}/{filename}')
+        pio.write_image(figure, filepath)
+        logger.info(f'Graph saved to {filepath}')
     except Exception as e:
         logger.exception(
             'Failed to generate graph. Fix the issue and run retro-analysis to generate graphs.')
