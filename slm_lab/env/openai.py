f29c44986cc7333bb68f5edd355927d0948f3b86
kengz <kengzwl@gmail.com>
2018-11-10 21:05:22 -0800

    uncover and apply even more hidden OpenAI baselines env wrappers

--- slm_lab/env/openai.py
+++ slm_lab/env/openai.py
@@ -17,10 +17,100 @@ def guard_reward(reward):
         return reward[0]
 
 
+# Series of custom Atari wrappers that don't come with gym but found hidden deep in OpenAI baselines
+# from https://github.com/openai/baselines/blob/master/baselines/common/atari_wrappers.py
+
+class NoopResetEnv(gym.Wrapper):
+    def __init__(self, env, noop_max=30):
+        '''Sample initial states by taking random number of no-ops on reset.
+        No-op is assumed to be action 0.
+        '''
+        gym.Wrapper.__init__(self, env)
+        self.noop_max = noop_max
+        self.override_num_noops = None
+        self.noop_action = 0
+        assert env.unwrapped.get_action_meanings()[0] == 'NOOP'
+
+    def reset(self, **kwargs):
+        ''' Do no-op action for a number of steps in [1, noop_max].'''
+        self.env.reset(**kwargs)
+        if self.override_num_noops is not None:
+            noops = self.override_num_noops
+        else:
+            noops = self.unwrapped.np_random.randint(1, self.noop_max + 1)  # pylint: disable=E1101
+        assert noops > 0
+        obs = None
+        for _ in range(noops):
+            obs, _, done, _ = self.env.step(self.noop_action)
+            if done:
+                obs = self.env.reset(**kwargs)
+        return obs
+
+    def step(self, ac):
+        return self.env.step(ac)
+
+
+class FireResetEnv(gym.Wrapper):
+    def __init__(self, env):
+        '''Take action on reset for environments that are fixed until firing.'''
+        gym.Wrapper.__init__(self, env)
+        assert env.unwrapped.get_action_meanings()[1] == 'FIRE'
+        assert len(env.unwrapped.get_action_meanings()) >= 3
+
+    def reset(self, **kwargs):
+        self.env.reset(**kwargs)
+        obs, _, done, _ = self.env.step(1)
+        if done:
+            self.env.reset(**kwargs)
+        obs, _, done, _ = self.env.step(2)
+        if done:
+            self.env.reset(**kwargs)
+        return obs
+
+    def step(self, ac):
+        return self.env.step(ac)
+
+
+class EpisodicLifeEnv(gym.Wrapper):
+    def __init__(self, env):
+        '''Make end-of-life == end-of-episode, but only reset on true game over.
+        Done by DeepMind for the DQN and co. since it helps value estimation.
+        '''
+        gym.Wrapper.__init__(self, env)
+        self.lives = 0
+        self.was_real_done = True
+
+    def step(self, action):
+        obs, reward, done, info = self.env.step(action)
+        self.was_real_done = done
+        # check current lives, make loss of life terminal,
+        # then update lives to handle bonus lives
+        lives = self.env.unwrapped.ale.lives()
+        if lives < self.lives and lives > 0:
+            # for Qbert sometimes we stay in lives == 0 condtion for a few frames
+            # so its important to keep lives > 0, so that we only reset once
+            # the environment advertises done.
+            done = True
+        self.lives = lives
+        return obs, reward, done, info
+
+    def reset(self, **kwargs):
+        '''Reset only when lives are exhausted.
+        This way all states are still reachable even though lives are episodic,
+        and the learner need not know about any of this behind-the-scenes.
+        '''
+        if self.was_real_done:
+            obs = self.env.reset(**kwargs)
+        else:
+            # no-op step to advance from terminal/lost life state
+            obs, _, _, _ = self.env.step(0)
+        self.lives = self.env.unwrapped.ale.lives()
+        return obs
+
+
 class MaxAndSkipEnv(gym.Wrapper):
     '''
     OpenAI max-skipframe wrapper from baselines (not available from gym itself)
-    from https://github.com/openai/baselines/blob/master/baselines/common/atari_wrappers.py
     '''
 
     def __init__(self, env, skip=4):
@@ -58,9 +148,15 @@ class OpenAIEnv(BaseEnv):
     def __init__(self, spec, e=None, env_space=None):
         super(OpenAIEnv, self).__init__(spec, e, env_space)
         register_env(spec)  # register any additional environments first
-        self.u_env = gym.make(self.name)
+        env = gym.make(self.name)
+        # apply the series of hidden env wrappers from OpenAI baselines
         if spec['env'][0]['name'].endswith('NoFrameskip-v4'):
-            self.u_env = MaxAndSkipEnv(self.u_env, skip=4)  # custom for Atari
+            env = NoopResetEnv(env, noop_max=30)
+            env = MaxAndSkipEnv(env, skip=4)
+            env = EpisodicLifeEnv(env)
+            if 'FIRE' in env.unwrapped.get_action_meanings():
+                env = FireResetEnv(env)
+        self.u_env = env
         self._set_attr_from_u_env(self.u_env)
         self.max_timestep = self.max_timestep or self.u_env.spec.tags.get('wrapper_config.TimeLimit.max_episode_steps')
         if env_space is None:  # singleton mode
