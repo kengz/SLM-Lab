b5b24981a69c651b93383f71b39550da0b71acda
kengz <kengzwl@gmail.com>
2018-11-29 23:35:29 -0800

    hard-wrap openai env for atari

--- slm_lab/env/openai.py
+++ slm_lab/env/openai.py
@@ -142,6 +142,29 @@ class MaxAndSkipEnv(gym.Wrapper):
         return self.env.reset(**kwargs)
 
 
+class ClippedRewardsWrapper(gym.RewardWrapper):
+    def reward(self, reward):
+        '''Atari reward, to -1, 0 or +1.'''
+        return np.sign(reward)
+
+
+def wrap_dqn(env, stack_frames=4, episodic_life=True, reward_clipping=True):
+    '''Apply a common set of wrappers for Atari games.'''
+    assert 'NoFrameskip' in env.spec.id
+    if episodic_life:
+        env = EpisodicLifeEnv(env)
+    env = NoopResetEnv(env, noop_max=30)
+    env = MaxAndSkipEnv(env, skip=4)
+    if 'FIRE' in env.unwrapped.get_action_meanings():
+        env = FireResetEnv(env)
+    # env = ProcessFrame84(env)
+    # env = ImageToPyTorch(env)
+    # env = FrameStack(env, stack_frames)
+    if reward_clipping:
+        env = ClippedRewardsWrapper(env)
+    return env
+
+
 class OpenAIEnv(BaseEnv):
     '''
     Wrapper for OpenAI Gym env to work with the Lab.
@@ -159,13 +182,14 @@ class OpenAIEnv(BaseEnv):
         super(OpenAIEnv, self).__init__(spec, e, env_space)
         register_env(spec)  # register any additional environments first
         env = gym.make(self.name)
+        env = wrap_dqn(env)
         # apply the series of hidden env wrappers from OpenAI baselines
-        if spec['env'][0]['name'].endswith('NoFrameskip-v4'):
-            env = NoopResetEnv(env, noop_max=30)
-            env = MaxAndSkipEnv(env, skip=4)
-            env = EpisodicLifeEnv(env)
-            if 'FIRE' in env.unwrapped.get_action_meanings():
-                env = FireResetEnv(env)
+        # if spec['env'][0]['name'].endswith('NoFrameskip-v4'):
+        #     env = NoopResetEnv(env, noop_max=30)
+        #     env = MaxAndSkipEnv(env, skip=4)
+        #     env = EpisodicLifeEnv(env)
+        #     if 'FIRE' in env.unwrapped.get_action_meanings():
+        #         env = FireResetEnv(env)
         self.u_env = env
         self._set_attr_from_u_env(self.u_env)
         self.max_t = self.max_t or self.u_env.spec.tags.get('wrapper_config.TimeLimit.max_epi_steps')
