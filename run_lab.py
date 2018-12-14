312965fb177875ee39eacaf6af8210fa66ab7742
lgraesser <lhgraesser@gmail.com>
2018-12-13 22:20:41 -0800

    Extend eval logic to enjoy mode

--- run_lab.py
+++ run_lab.py
@@ -57,22 +57,23 @@ def run_by_mode(spec_file, spec_name, lab_mode):
         else:
             info_space.tick('trial')
         Trial(spec, info_space).run()
-    elif lab_mode.startswith('enjoy') or lab_mode.startswith('eval'):
+    elif lab_mode.startswith('enjoy'):
         prepath = lab_mode.split('@')[1]
-        # For eval mode we create two InfoSpaces, the original and
-        # a new one, and copy all the relevant data (spec and models)
-        # to the new InfoSpace and use this. The original experiment
-        # folder remains unchanged
         new_info_space = deepcopy(info_space)
         spec, info_space = util.prepath_to_spec_info_space(prepath)
-        if lab_mode.startswith('eval'):
-            new_spec = util.override_eval_spec(deepcopy(spec))
-            util.prepare_eval_directory(new_spec, new_info_space, spec, info_space, prepath)
-            logger.info('Eval directory prepared')
-            new_info_space.tick('trial')
-            Trial(new_spec, new_info_space).run()
-        else:
-            Session(spec, info_space).run()
+        new_spec = util.override_enjoy_spec(deepcopy(spec))
+        util.prepare_directory(new_spec, new_info_space, spec, info_space, prepath)
+        new_info_space.tick('trial')
+        new_info_space.tick('session')
+        Session(new_spec, new_info_space).run()
+    elif lab_mode.startswith('eval'):
+        prepath = lab_mode.split('@')[1]
+        new_info_space = deepcopy(info_space)
+        spec, info_space = util.prepath_to_spec_info_space(prepath)
+        new_spec = util.override_eval_spec(deepcopy(spec))
+        util.prepare_directory(new_spec, new_info_space, spec, info_space, prepath)
+        new_info_space.tick('trial')
+        Trial(new_spec, new_info_space).run()
     elif lab_mode == 'dev':
         spec = util.override_dev_spec(spec)
         info_space.tick('trial')
