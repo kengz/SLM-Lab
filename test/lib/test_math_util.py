6cd307dbd00e070a06b79615042099dec2f2e37b
lgraesser <lhgraesser@gmail.com>
2018-12-15 16:21:34 -0800

    Rate decay unit test

--- test/lib/test_math_util.py
+++ test/lib/test_math_util.py
@@ -16,3 +16,23 @@ import pytest
 ])
 def test_linear_decay(start_val, end_val, start_step, end_step, step, correct):
     assert math_util.linear_decay(start_val, end_val, start_step, end_step, step) == correct
+
+
+@pytest.mark.parametrize('start_val, end_val, start_step, end_step, step, correct', [
+    (1.0, 0.0, 0, 100, 0, 1.0),
+    (1.0, 0.0, 0, 100, 5, 0.9),
+    (1.0, 0.0, 0, 100, 10, 0.81),
+    (1.0, 0.0, 0, 100, 25, 0.59049),
+    (1.0, 0.0, 0, 100, 50, 0.3486784401),
+    (1.0, 0.0, 0, 100, 100, 0.0),
+    (1.0, 0.0, 0, 100, 150, 0.0),
+    (1.0, 0.0, 100, 200, 0, 1.0),
+    (1.0, 0.0, 100, 200, 50, 1.0),
+    (1.0, 0.0, 100, 200, 100, 1.0),
+    (1.0, 0.0, 100, 200, 105, 0.9),
+    (1.0, 0.0, 100, 200, 125, 0.59049),
+    (1.0, 0.0, 100, 200, 200, 0.0),
+    (1.0, 0.0, 100, 200, 250, 0.0),
+])
+def test_rate_decay(start_val, end_val, start_step, end_step, step, correct):
+    np.testing.assert_almost_equal(math_util.rate_decay(start_val, end_val, start_step, end_step, step), correct)
