eefb2db6b44b13a36bb200cd44384e130bac39ed
lgraesser <lhgraesser@gmail.com>
2018-12-15 16:11:32 -0800

    Add test for linear_decay

--- test/lib/test_math_util.py
+++ test/lib/test_math_util.py
@@ -1,3 +1,18 @@
 from slm_lab.lib import math_util
 import numpy as np
 import pytest
+
+
+@pytest.mark.parametrize('start_val, end_val, start_step, end_step, step, correct', [
+    (0.1, 0.0, 0, 100, 0, 0.1),
+    (0.1, 0.0, 0, 100, 50, 0.05),
+    (0.1, 0.0, 0, 100, 100, 0.0),
+    (0.1, 0.0, 0, 100, 150, 0.0),
+    (0.1, 0.0, 100, 200, 50, 0.1),
+    (0.1, 0.0, 100, 200, 100, 0.1),
+    (0.1, 0.0, 100, 200, 150, 0.05),
+    (0.1, 0.0, 100, 200, 200, 0.0),
+    (0.1, 0.0, 100, 200, 250, 0.0),
+])
+def test_linear_decay(start_val, end_val, start_step, end_step, step, correct):
+    assert math_util.linear_decay(start_val, end_val, start_step, end_step, step) == correct
