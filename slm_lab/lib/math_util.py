1f131f4c7485b83e4fe690dbc67b56579a74621e
lgraesser <lhgraesser@gmail.com>
2018-12-15 16:02:29 -0800

    Offset by start_step

--- slm_lab/lib/math_util.py
+++ slm_lab/lib/math_util.py
@@ -126,7 +126,7 @@ def linear_decay(start_val, end_val, start_step, end_step, step):
     if step < start_step:
         return start_val
     slope = (end_val - start_val) / (end_step - start_step)
-    val = max(slope * step + start_val, end_val)
+    val = max(slope * (step - start_step) + start_val, end_val)
     return val
 
 
@@ -135,7 +135,7 @@ def rate_decay(start_val, end_val, start_step, end_step, step, decay_rate=0.9, f
     if step < start_step:
         return start_val
     step_per_decay = (end_step - start_step) / frequency
-    decay_step = step / step_per_decay
+    decay_step = (step - start_step) / step_per_decay
     val = max(np.power(decay_rate, decay_step) * start_val, end_val)
     return val
 
@@ -151,7 +151,7 @@ def periodic_decay(start_val, end_val, start_step, end_step, step, frequency=60.
         return start_val
     x_freq = frequency
     step_per_decay = (end_step - start_step) / x_freq
-    x = step / step_per_decay
+    x = (step - start_step) / step_per_decay
     unit = start_val - end_val
     val = end_val * 0.5 * unit * (1 + np.cos(x) * (1 - x / x_freq))
     val = max(val, end_val)
