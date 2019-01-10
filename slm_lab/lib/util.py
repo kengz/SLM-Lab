6f312ee3cb7b8b3795b9daeee220465a70a8119f
kengz <kengzwl@gmail.com>
2019-01-09 22:46:04 -0800

    remove pool init from parallelize

--- slm_lab/lib/util.py
+++ slm_lab/lib/util.py
@@ -303,11 +303,7 @@ def parallelize_fn(fn, args, num_cpus=NUM_CPUS):
     fn should take only a single arg.
     @returns {list} results Order preserved output from fn.
     '''
-    def pool_init():
-        # you can never be too safe in multiprocessing gc
-        import gc
-        gc.collect()
-    pool = mp.Pool(num_cpus, initializer=pool_init, maxtasksperchild=1)
+    pool = mp.Pool(num_cpus, maxtasksperchild=1)
     results = pool.map(fn, args)
     pool.close()
     pool.join()
