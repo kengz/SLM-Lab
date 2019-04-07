225a00e41c52f1f93a89e23278d3235e6c2c71d8
lgraesser <lhgraesser@gmail.com>
2019-04-07 02:46:11 -0700

    Fix image shape

--- slm_lab/env/vec_wrapper.py
+++ slm_lab/env/vec_wrapper.py
@@ -490,8 +490,8 @@ class VecFrameStack(VecEnvWrapper):
         self.venv = venv
         self.nstack = nstack
         wos = venv.observation_space  # wrapped ob space
-        low = np.repeat(wos.low, self.nstack, axis=-1)
-        high = np.repeat(wos.high, self.nstack, axis=-1)
+        low = np.repeat(wos.low, self.nstack, axis=0)
+        high = np.repeat(wos.high, self.nstack, axis=0)
         self.stackedobs = np.zeros((venv.num_envs,) + low.shape, low.dtype)
         observation_space = spaces.Box(low=low, high=high, dtype=venv.observation_space.dtype)
         VecEnvWrapper.__init__(self, venv, observation_space=observation_space)
