9a4c9c20cf9c5be0e09b2a7446074a6c2291fb61
kengz <kengzwl@gmail.com>
2019-04-19 09:19:30 -0700

    use -n 2 process to prevent timeout in ci test

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 from setuptools.command.test import test as TestCommand
 
 test_args = [
-    '-n 4',
+    '-n 2',
     '--verbose',
     '--capture=sys',
     '--log-level=INFO',
