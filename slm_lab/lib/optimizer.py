be5bc1257ea37d904b725adc0397d8024b043d84
kengz <kengzwl@gmail.com>
2019-05-18 11:42:06 -0700

    add GlobalRMSProp

--- slm_lab/lib/optimizer.py
+++ slm_lab/lib/optimizer.py
@@ -51,6 +51,51 @@ class GlobalAdam(torch.optim.Adam):
                 bias_correction2 = 1 - beta2 ** state['step'].item()
                 step_size = group['lr'] * math.sqrt(
                     bias_correction2) / bias_correction1
-
                 p.data.addcdiv_(-step_size, exp_avg, denom)
+        return loss
+
+
+class GlobalRMSprop(torch.optim.RMSprop):
+    '''
+    Global RMSprop algorithm with shared states for Hogwild.
+    Adapted from https://github.com/jingweiz/pytorch-rl/blob/master/optims/sharedRMSprop.py (MIT)
+    '''
+
+    def __init__(self, params, lr=1e-2, alpha=0.99, eps=1e-8, weight_decay=0):
+        super().__init__(params, lr=lr, alpha=alpha, eps=eps, weight_decay=weight_decay, momentum=0, centered=False)
+
+        # State initialisation (must be done before step, else will not be shared between threads)
+        for group in self.param_groups:
+            for p in group['params']:
+                state = self.state[p]
+                state['step'] = p.data.new().resize_(1).zero_()
+                state['square_avg'] = p.data.new().resize_as_(p.data).zero_()
+
+    def share_memory(self):
+        for group in self.param_groups:
+            for p in group['params']:
+                state = self.state[p]
+                state['step'].share_memory_()
+                state['square_avg'].share_memory_()
+
+    def step(self, closure=None):
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                grad = p.grad.data
+                state = self.state[p]
+                square_avg = state['square_avg']
+                alpha = group['alpha']
+                state['step'] += 1
+                if group['weight_decay'] != 0:
+                    grad = grad.add(group['weight_decay'], p.data)
+
+                square_avg.mul_(alpha).addcmul_(1 - alpha, grad, grad)
+                avg = square_avg.sqrt().add_(group['eps'])
+                p.data.addcdiv_(-group['lr'], grad, avg)
         return loss
