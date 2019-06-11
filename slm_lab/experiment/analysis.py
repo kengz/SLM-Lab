73f1d3586038c9aa554a74914a920fb3a0e30ead
kengz <kengzwl@gmail.com>
2019-06-10 21:07:13 -0700

    insert trial index to experiment_df

--- slm_lab/experiment/analysis.py
+++ slm_lab/experiment/analysis.py
@@ -220,6 +220,8 @@ def calc_experiment_df(trial_data_dict, info_prepath=None):
     sorted_cols = config_cols + cols
     experiment_df = experiment_df.reindex(sorted_cols, axis=1)
     experiment_df.sort_values(by=['strength'], ascending=False, inplace=True)
+    # insert trial index
+    experiment_df.insert(0, 'trial', experiment_df.index)
     if info_prepath is not None:
         util.write(experiment_df, f'{info_prepath}_experiment_df.csv')
         # save important metrics in info_prepath directly
