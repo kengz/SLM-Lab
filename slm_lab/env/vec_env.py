ad564fa72a624a29fbe7ea4bd5e387ca7f8fed4a
kengz <kengzwl@gmail.com>
2019-07-27 18:22:49 -0700

    restore reward_scale from vec to low level

--- slm_lab/env/vec_env.py
+++ slm_lab/env/vec_env.py
@@ -450,13 +450,11 @@ class ShmemVecEnv(VecEnv):
 class VecFrameStack(VecEnvWrapper):
     '''Frame stack wrapper for vector environment'''
 
-    def __init__(self, venv, frame_op, frame_op_len, reward_scale=None):
+    def __init__(self, venv, frame_op, frame_op_len):
         self.venv = venv
         assert frame_op in ('concat', 'stack'), 'Invalid frame_op mode'
         self.is_stack = frame_op == 'stack'
         self.frame_op_len = frame_op_len
-        self.reward_scale = reward_scale
-        self.sign_reward = self.reward_scale == 'sign'
         self.spec = venv.spec
         wos = venv.observation_space  # wrapped ob space
         if self.is_stack:
@@ -495,7 +493,7 @@ def make_gym_venv(name, num_envs=4, seed=0, frame_op=None, frame_op_len=None, re
     '''General method to create any parallel vectorized Gym env; auto wraps Atari'''
     venv = [
         # don't concat frame or clip reward on individual env; do that at vector level
-        partial(make_gym_env, name, seed + i, frame_op=None, frame_op_len=None, reward_scale=None, normalize_state=normalize_state, episode_life=episode_life)
+        partial(make_gym_env, name, seed + i, frame_op=None, frame_op_len=None, reward_scale=reward_scale, normalize_state=normalize_state, episode_life=episode_life)
         for i in range(num_envs)
     ]
     if len(venv) > 1:
@@ -503,5 +501,5 @@ def make_gym_venv(name, num_envs=4, seed=0, frame_op=None, frame_op_len=None, re
     else:
         venv = DummyVecEnv(venv)
     if frame_op is not None:
-        venv = VecFrameStack(venv, frame_op, frame_op_len, reward_scale)
+        venv = VecFrameStack(venv, frame_op, frame_op_len)
     return venv
