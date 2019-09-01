42a0d3b0ae4b7b5f6d94aed0998b278ae8350263
kengz <kengzwl@gmail.com>
2019-09-01 00:59:46 -0700

    update humanoid benchmark

--- bin/plot_script.py
+++ bin/plot_script.py
@@ -26,9 +26,9 @@ viz.plot_multi_trial(trial_metrics_path_list, legend_list, title, graph_prepath,
 # Roboschool
 env_list = [
     'RoboschoolAnt',
-    # 'RoboschoolAtlasForwardWalk',
+    'RoboschoolAtlasForwardWalk',
     'RoboschoolHalfCheetah',
-    # 'RoboschoolHopper',
+    'RoboschoolHopper',
     'RoboschoolInvertedDoublePendulum',
     'RoboschoolInvertedPendulum',
     'RoboschoolReacher',
@@ -40,7 +40,7 @@ for env in env_list:
         f'data/a2c_gae_roboschool_{env}-v1_2019_08_27_135211/info/a2c_gae_roboschool_{env}-v1_t0_trial_metrics.pkl',
         f'data/a2c_nstep_roboschool_{env}-v1_2019_08_27_075653/info/a2c_nstep_roboschool_{env}-v1_t0_trial_metrics.pkl',
         f'data/ppo_roboschool_{env}-v1_2019_08_27_182010/info/ppo_roboschool_{env}-v1_t0_trial_metrics.pkl',
-        f'data/sac_roboschool_{env}-v1_2019_08_29_072253/info/sac_roboschool_{env}-v1_t0_trial_metrics.pkl',
+        f'data/sac_roboschool_{env}-v1_2019_08_29_021001/info/sac_roboschool_{env}-v1_t0_trial_metrics.pkl',
     ]
     legend_list = [
         'A2C (GAE)',
@@ -52,3 +52,60 @@ for env in env_list:
     graph_prepath = f'data/almanac_{env}'
     viz.plot_multi_trial(trial_metrics_path_list, legend_list, title, graph_prepath)
     viz.plot_multi_trial(trial_metrics_path_list, legend_list, title, graph_prepath, ma=True)
+
+
+# Humanoid
+
+env = 'humanoid'
+trial_metrics_path_list = [
+    f'data/a2c_gae_{env}_2019_08_27_170720/info/a2c_gae_{env}_t0_trial_metrics.pkl',
+    f'data/a2c_nstep_{env}_2019_08_27_170715/info/a2c_nstep_{env}_t0_trial_metrics.pkl',
+    f'data/ppo_{env}_2019_08_27_170710/info/ppo_{env}_t0_trial_metrics.pkl',
+    f'data/async_sac_{env}_2019_08_29_164833/info/async_sac_{env}_t0_trial_metrics.pkl',
+]
+legend_list = [
+    'A2C (GAE)',
+    'A2C (n-step)',
+    'PPO',
+    'SAC',
+]
+title = f'multi trial graph: {env}'
+graph_prepath = f'data/almanac_{env}'
+viz.plot_multi_trial(trial_metrics_path_list, legend_list, title, graph_prepath)
+viz.plot_multi_trial(trial_metrics_path_list, legend_list, title, graph_prepath, ma=True)
+
+env = 'humanoidflagrun'
+trial_metrics_path_list = [
+    f'data/a2c_gae_{env}_2019_08_28_041224/info/a2c_gae_{env}_t0_trial_metrics.pkl',
+    f'data/a2c_nstep_{env}_2019_08_28_041251/info/a2c_nstep_{env}_t0_trial_metrics.pkl',
+    f'data/ppo_{env}_2019_08_28_041328/info/ppo_{env}_t0_trial_metrics.pkl',
+    f'data/async_sac_{env}_2019_08_29_164843/info/async_sac_{env}_t0_trial_metrics.pkl',
+]
+legend_list = [
+    'A2C (GAE)',
+    'A2C (n-step)',
+    'PPO',
+    'SAC',
+]
+title = f'multi trial graph: {env}'
+graph_prepath = f'data/almanac_{env}'
+viz.plot_multi_trial(trial_metrics_path_list, legend_list, title, graph_prepath)
+viz.plot_multi_trial(trial_metrics_path_list, legend_list, title, graph_prepath, ma=True)
+
+env = 'humanoidflagrunharder'
+trial_metrics_path_list = [
+    f'data/a2c_gae_{env}_2019_08_28_041503/info/a2c_gae_{env}_t0_trial_metrics.pkl',
+    f'data/a2c_nstep_{env}_2019_08_28_041525/info/a2c_nstep_{env}_t0_trial_metrics.pkl',
+    f'data/ppo_{env}_2019_08_28_041447/info/ppo_{env}_t0_trial_metrics.pkl',
+    f'data/async_sac_{env}_2019_08_29_164837/info/async_sac_{env}_t0_trial_metrics.pkl',
+]
+legend_list = [
+    'A2C (GAE)',
+    'A2C (n-step)',
+    'PPO',
+    'SAC',
+]
+title = f'multi trial graph: {env}'
+graph_prepath = f'data/almanac_{env}'
+viz.plot_multi_trial(trial_metrics_path_list, legend_list, title, graph_prepath)
+viz.plot_multi_trial(trial_metrics_path_list, legend_list, title, graph_prepath, ma=True)
