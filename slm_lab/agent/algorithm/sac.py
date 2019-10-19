f47f96b4a720cca57550771ae7a34a1653a7efb1
kengz <kengzwl@gmail.com>
2019-10-19 12:19:16 -0700

    adjust clamp range

--- slm_lab/agent/algorithm/sac.py
+++ slm_lab/agent/algorithm/sac.py
@@ -186,8 +186,8 @@ class SoftActorCritic(ActorCritic):
         self.alpha_optim.zero_grad()
         alpha_loss.backward()
         self.alpha_optim.step()
-        with torch.no_grad():  # clamp alpha range to ~ 0.018 to 54
-            self.log_alpha.clamp_(-4, 4)
+        with torch.no_grad():  # clamp alpha range to ~ 0.0003 to 54
+            self.log_alpha.clamp_(-8, 4)
         self.alpha = self.log_alpha.detach().exp()
         self.body.tb_tracker['alpha'] = self.alpha
 
