4be0081e200103c1c57bdbd3aaadebcd8bf04e23
kengz <kengzwl@gmail.com>
2020-03-02 00:01:46 -0800

    correct size shrink in transformer

--- slm_lab/agent/net/transformer.py
+++ slm_lab/agent/net/transformer.py
@@ -27,14 +27,52 @@ class PositionalEncoding(nn.Module):
         return self.dropout(x)
 
 
+class PosExpand(nn.Module):
+    '''Construct the positional encoding and pass through an expansion layer.'''
+    def __init__(self, in_dim, num_hids, dropout):
+        super().__init__()
+        self.pos_encoder = PositionalEncoding(in_dim, dropout)
+        self.expander = nn.Linear(in_dim, num_hids)
+
+    def forward(self, x):
+        x = self.pos_encoder(x)
+        x = self.expander(x)
+        return x
+
+
+class PosEmbedding(nn.Module):
+    '''Construct the embedding from input (state) and position embedding.'''
+
+    def __init__(self, in_dim, num_hids, dropout):
+        super().__init__()
+        self.in_embedding = nn.Linear(in_dim, num_hids)
+        self.position_embedding = nn.Embedding(in_dim, num_hids)
+        self.LayerNorm = nn.LayerNorm(num_hids, eps=1e-12)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        seq_len = x.shape[1]
+        position_ids = torch.arange(seq_len, dtype=torch.long, device=x.device)
+        position_ids = position_ids.unsqueeze(0).expand(x.shape[:2])
+
+        inputs_embeds = self.in_embedding(x)
+        position_embedding = self.position_embedding(position_ids)
+
+        embedding = inputs_embeds + position_embedding
+        embedding = self.LayerNorm(embedding)
+        embedding = self.dropout(embedding)
+        return embedding
+
+
 class Transformer(nn.Module):
     '''The transformer for RL: only the encoder and multihead attention'''
 
     def __init__(self, in_dim, out_dim, num_heads, num_hids, num_layers, dropout=0.5):
         super(Transformer, self).__init__()
         self.src_mask = None
-        self.pos_encoder = PositionalEncoding(in_dim, dropout)
-        encoder_layers = TransformerEncoderLayer(in_dim, num_heads, num_hids, dropout)
+        self.embedding = PosEmbedding(in_dim, num_hids, dropout)
+        # self.embedding = PosExpand(in_dim, num_hids, dropout)
+        encoder_layers = TransformerEncoderLayer(num_hids, num_heads, num_hids, dropout)
         self.transformer_encoder = TransformerEncoder(encoder_layers, num_layers)
         self.in_dim = in_dim
 
@@ -43,13 +81,12 @@ class Transformer(nn.Module):
         mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
         return mask
 
-    def forward(self, src):
-        if self.src_mask is None or self.src_mask.size(0) != len(src):
-            device = src.device
-            mask = self._generate_square_subsequent_mask(len(src)).to(device)
+    def forward(self, x):
+        if self.src_mask is None or self.src_mask.size(0) != len(x):
+            mask = self._generate_square_subsequent_mask(len(x)).to(x.device)
             self.src_mask = mask
-        src = self.pos_encoder(src)
-        output = self.transformer_encoder(src, self.src_mask)
+        x = self.embedding(x)
+        output = self.transformer_encoder(x, self.src_mask)
         return output
 
 
@@ -95,14 +132,14 @@ class TransformerNet(Net, nn.Module):
         self.model = Transformer(in_dim=in_dim, out_dim=self.out_dim, num_heads=self.num_heads, num_hids=self.num_hids, num_layers=self.num_layers, dropout=self.dropout)
         # usual tail architecture like MLP
         if ps.is_integer(self.out_dim):
-            self.model_tail = net_util.build_fc_model([in_dim, self.out_dim], self.out_layer_activation)
+            self.model_tail = net_util.build_fc_model([self.num_hids, self.out_dim], self.out_layer_activation)
         else:
             if not ps.is_list(self.out_layer_activation):
                 self.out_layer_activation = [self.out_layer_activation] * len(out_dim)
             assert len(self.out_layer_activation) == len(self.out_dim)
             tails = []
             for out_d, out_activ in zip(self.out_dim, self.out_layer_activation):
-                tail = net_util.build_fc_model([in_dim, out_d], out_activ)
+                tail = net_util.build_fc_model([self.num_hids, out_d], out_activ)
                 tails.append(tail)
             self.model_tails = nn.ModuleList(tails)
 
@@ -112,13 +149,12 @@ class TransformerNet(Net, nn.Module):
 
     def forward(self, x):
         '''The feedforward step'''
-        x = x.transpose(0, 1)  # batch-first into seq-first
-        x = self.model(x)
+        x = self.model(x)[:, 0]  # batch-first, get first in seq like BERT
         if hasattr(self, 'model_tails'):
             outs = []
             for model_tail in self.model_tails:
-                outs.append(model_tail(x)[0, :, :])  # get first like BERT
+                outs.append(model_tail(x))
             return outs
         else:
-            out = self.model_tail(x)[0, :, :]  # get first like BERT
+            out = self.model_tail(x)
             return out
