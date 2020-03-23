3d8cd6ea9842aa25b13c5c1fff92258de0de45fc
kengz <kengzwl@gmail.com>
2020-03-23 10:48:30 -0400

    use manual orca shutdown to avoid zombie processes

--- slm_lab/lib/viz.py
+++ slm_lab/lib/viz.py
@@ -10,7 +10,6 @@ import pydash as ps
 
 logger = logger.get_logger(__name__)
 pio.templates.default = 'none'  # set default white background for plots
-pio.orca.config.timeout = 30  # shutdown orca server after 30s inactivity
 # moving-average window size for plotting
 PLOT_MA_WINDOW = 100
 # warn orca failure only once
