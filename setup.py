9fe38b28b5b1991c5aaba1f449385fe8c3c5e604
kengz <kengzwl@gmail.com>
2021-12-04 18:52:41 -0500

    chore(ci): remove unused CI env vars

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class PyTest(TestCommand):
 
 setup(
     name='slm_lab',
-    version='4.0.1',
+    version='4.2.3',
     description='Modular Deep Reinforcement Learning framework in PyTorch.',
     long_description='https://github.com/kengz/slm_lab',
     keywords='SLM Lab',
